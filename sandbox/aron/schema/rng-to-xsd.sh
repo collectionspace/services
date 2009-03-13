cbe1a272e5c45069c40f4097700499f01a767204
Aron Roberts <aron@socrates.berkeley.edu>
2009-03-13 19:59:11 +0000

    CSPACE-12: Added a 'full' collectionObject schema in RELAX NG that includes, rather than incorporating by reference, the serviceMetadata and defaultCollectionObject schemas.  This, in turn, was used as input to the 'Trang' utility to generate a W3C XML Schema (XSD) version of the collection object schema.  Also included two trivial utility scripts to wrap RELAX NG validation and RELAX NG-to-XSD generation. NB: the XSD version of the collection object schema included with this commit does not include a CSpace- or Hello World-specific namespace.

---
+++ sandbox/aron/schema/rng-to-xsd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# rng-to-xsd.sh
+# 
+# Simple bash shell script to generate a W3C XML Schema (XSD) document
+# from a RELAX NG schema document.
+#
+# Requires two arguments:
+# - Filename of the RELAX NG schema document.
+#   (or the full path to this document, if it is not in the current directory)
+# - Filename of the new XML instance document to be created.
+#   (or the full path to this document, if it is not in the current directory)
+#
+# $Author: aron $
+# $Revision: 57 $
+# $Date: 2009-03-05 16:06:06 -0800 (Thu, 05 Mar 2009) $
+
+java -jar tools/trang.jar -I rng -O xsd $1 $2
---
+++ sandbox/aron/schema/validate-rng.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Simple bash shell script to validate an XML instance document
+# against a RELAX NG schema.
+#
+# Requires two arguments:
+# - Filename of the RELAX NG schema document.
+#   (or the full path to this document, if it is not in the current directory)
+# - Filename of the XML instance document
+#   (or the full path to this document, if it is not in the current directory)
+#
+# $Author: aron $
+# $Revision: 57 $
+# $Date: 2009-03-05 16:06:06 -0800 (Thu, 05 Mar 2009) $
+
+java -jar tools/jing.jar $1 $2
