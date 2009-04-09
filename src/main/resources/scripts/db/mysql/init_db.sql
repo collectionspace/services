5813cc96bb6e39e03314cbb2635343859457c365
Sanjay Dalal <sanjay.dalal@berkeley.edu>
2009-04-09 19:24:00 +0000

    CSPACE-61 added database initialization script for mysql

---
+++ src/main/resources/scripts/db/mysql/init_db.sql
@@ -0,0 +1,36 @@
+﻿--
+-- Create and initialize CollectionSpace service database environment for MySQL
+--
+
+--
+-- delete all users
+--
+DELETE FROM mysql.user WHERE NOT (host="localhost" and user="root");
+FLUSH PRIVILEGES;
+
+--
+-- delete anonymous access
+--
+DELETE FROM mysql.user WHERE User = '';
+FLUSH PRIVILEGES;
+
+--
+-- recreate jbossdb database
+--
+DROP database IF EXISTS jbossdb;
+CREATE database jbossdb;
+
+--
+-- recreate nuxeo database
+--
+DROP database IF EXISTS nuxeo;
+CREATE database nuxeo;
+
+--
+-- grant privileges to test user on nuxeo and jbossdb databases
+--
+GRANT ALL PRIVILEGES ON jbossdb.* TO 'test'@'localhost' IDENTIFIED BY 'test' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON nuxeo.* TO 'test'@'localhost' IDENTIFIED BY 'test' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
