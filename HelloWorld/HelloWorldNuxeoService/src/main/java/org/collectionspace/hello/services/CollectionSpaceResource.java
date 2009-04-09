99d86c5ebc3d26422680f399db15529cea19f8ac
Richard Millet <richard.millet@berkeley.edu>
2009-04-09 02:37:59 +0000

    Adding this interface for common/global values and settings to all CollectionSpace service resources.  Currently contains the hardcoded Nuxeo workspace UUID.

---
+++ HelloWorld/HelloWorldNuxeoService/src/main/java/org/collectionspace/hello/services/CollectionSpaceResource.java
@@ -0,0 +1,6 @@
+package org.collectionspace.hello.services;
+
+public interface CollectionSpaceResource {
+	static String CS_NUXEO_WORKSPACE_UID = "322e1045-8e6f-4984-b42a-f18fb817d091";
+	static String CS_NUXEO_DEFAULT_REPOS = "default";	
+}
