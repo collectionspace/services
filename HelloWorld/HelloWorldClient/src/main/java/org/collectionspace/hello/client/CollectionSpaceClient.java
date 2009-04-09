0d05be04fe491050612d33f5e1a7d19a969b3cfd
Richard Millet <richard.millet@berkeley.edu>
2009-04-09 02:52:09 +0000

    This is a place for common/global values for all CollectionSpace client classes.  Currently isolates hardcoded URL to service application.

---
+++ HelloWorld/HelloWorldClient/src/main/java/org/collectionspace/hello/client/CollectionSpaceClient.java
@@ -0,0 +1,6 @@
+package org.collectionspace.hello.client;
+
+public interface CollectionSpaceClient {
+	static final String HOST = "http://localhost:8080";
+	static final String URI = "/helloworld/cspace-nuxeo";
+}
