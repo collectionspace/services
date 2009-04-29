5e686e71ba3b9fe87e78785816f129e8199ee46b
Richard Millet <richard.millet@berkeley.edu>
2009-04-29 06:11:54 +0000

    CSPACE-84: Created a new source structure for all services.  Moved v0.1 CollectionObject service to this new structure.

---
+++ services/common/src/main/java/org/collectionspace/services/nuxeo/NuxeoCallback.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.collectionspace.services.nuxeo;
+
+
+import javax.security.auth.callback.Callback;
+
+/**
+ * Copied from jbossx
+ *
+ * An implementation of Callback that simply obtains an Object to be used
+ * as the authentication credential. Interpretation of the Object is up to
+ * the LoginModules that validate the credential.
+ *
+ * @author  Scott.Stark@jboss.org
+ */
+public class NuxeoCallback implements Callback {
+
+    private final String prompt;
+
+    private Object credential;
+
+    public NuxeoCallback() {
+        this("");
+    }
+
+    public NuxeoCallback(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public Object getCredential() {
+        return credential;
+    }
+
+    public void setCredential(Object credential) {
+        this.credential = credential;
+    }
+
+    public void clearCredential() {
+        credential = null;
+    }
+
+}
