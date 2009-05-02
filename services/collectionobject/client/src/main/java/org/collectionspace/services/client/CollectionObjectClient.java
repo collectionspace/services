5509d6ade57d3047c55c56f188ade1f11fb29847
Richard Millet <richard.millet@berkeley.edu>
2009-05-02 05:23:27 +0000

    CSPACE-84: Adding Client Library code to new source structure.

---
+++ services/collectionobject/client/src/main/java/org/collectionspace/services/client/CollectionObjectClient.java
@@ -0,0 +1,94 @@
+package org.collectionspace.services.client;
+
+import javax.ws.rs.core.Response;
+
+import org.collectionspace.services.collectionobject.CollectionObject;
+import org.collectionspace.services.collectionobject.CollectionObjectList;
+
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
+/**
+ * A CollectionObjectClient.
+
+ * @version $Revision:$
+ */
+public class CollectionObjectClient extends CollectionSpaceClient {
+
+
+    /**
+     *
+     */
+    private static final CollectionObjectClient instance = new CollectionObjectClient();
+    /**
+     *
+     */
+    private CollectionObjectProxy collectionObjectProxy;
+
+    /**
+     *
+     * Default constructor for CollectionObjectClient class.
+     *
+     */
+    private CollectionObjectClient() {
+        ResteasyProviderFactory factory = ResteasyProviderFactory.getInstance();
+        RegisterBuiltin.register(factory);
+        collectionObjectProxy = ProxyFactory.create(CollectionObjectProxy.class, getURL());
+    }
+
+    /**
+     * FIXME Comment this
+     *
+     * @return
+     */
+    public static CollectionObjectClient getInstance() {
+        return instance;
+    }
+
+    /**
+     * @return
+     * @see org.collectionspace.hello.client.CollectionObjectProxy#getCollectionObject()
+     */
+    public ClientResponse<CollectionObjectList> getCollectionObjectList() {
+        return collectionObjectProxy.getCollectionObjectList();
+    }
+
+    /**
+     * @param csid
+     * @return
+     * @see org.collectionspace.hello.client.CollectionObjectProxy#getCollectionObject(java.lang.String)
+     */
+    public ClientResponse<CollectionObject> getCollectionObject(String csid) {
+        return collectionObjectProxy.getCollectionObject(csid);
+    }
+
+    /**
+     * @param collectionobject
+     * @return
+     * @see org.collectionspace.hello.client.CollectionObjectProxy#createCollectionObject(org.collectionspace.hello.CollectionObject)
+     */
+    public ClientResponse<Response> createCollectionObject(CollectionObject collectionObject) {
+        return collectionObjectProxy.createCollectionObject(collectionObject);
+    }
+
+    /**
+     * @param csid
+     * @param collectionobject
+     * @return
+     * @see org.collectionspace.hello.client.CollectionObjectProxy#updateCollectionObject(java.lang.Long, org.collectionspace.hello.CollectionObject)
+     */
+    public ClientResponse<CollectionObject> updateCollectionObject(String csid, CollectionObject collectionObject) {
+        return collectionObjectProxy.updateCollectionObject(csid, collectionObject);
+    }
+
+    /**
+     * @param csid
+     * @return
+     * @see org.collectionspace.hello.client.CollectionObjectProxy#deleteCollectionObject(java.lang.Long)
+     */
+    public ClientResponse<Response> deleteCollectionObject(String csid) {
+        return collectionObjectProxy.deleteCollectionObject(csid);
+    }
+}
---
+++ services/collectionobject/client/src/main/java/org/collectionspace/services/client/CollectionObjectProxy.java
@@ -0,0 +1,46 @@
+package org.collectionspace.services.client;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.collectionspace.services.collectionobject.CollectionObject;
+import org.collectionspace.services.collectionobject.CollectionObjectList;
+import org.jboss.resteasy.client.ClientResponse;
+
+/**
+ * @version $Revision:$
+ */
+@Path("/collectionobjects/")
+@Produces({"application/xml"})
+@Consumes({"application/xml"})
+public interface CollectionObjectProxy {
+
+    @GET
+    ClientResponse<CollectionObjectList> getCollectionObjectList();
+
+    //(C)reate
+    @POST
+    ClientResponse<Response> createCollectionObject(CollectionObject co);
+
+    //(R)ead
+    @GET
+    @Path("/{csid}")
+    ClientResponse<CollectionObject> getCollectionObject(@PathParam("csid") String csid);
+
+    //(U)pdate
+    @PUT
+    @Path("/{csid}")
+    ClientResponse<CollectionObject> updateCollectionObject(@PathParam("csid") String csid, CollectionObject co);
+
+    //(D)elete
+    @DELETE
+    @Path("/{csid}")
+    ClientResponse<Response> deleteCollectionObject(@PathParam("csid") String csid);
+}
