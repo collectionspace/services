1810a777f205979dbc1931a3874b56ddde65eebb
Richard Millet <richard.millet@berkeley.edu>
2009-11-12 18:26:46 +0000

    CSPACE-593 Move Vocabulary Importer class out of "client" module and into a new module named "import" immediately under "/services/vocabulary" directory

--- services/vocabulary/import/src/main/java/org/collectionspace/services/vocabulary/importer/VocabularyBaseImport.java
+++ services/vocabulary/import/src/main/java/org/collectionspace/services/vocabulary/importer/VocabularyBaseImport.java
@@ -21,7 +21,7 @@
  * limitations under the License.
  */
 
-package org.collectionspace.services.client.importer;
+package org.collectionspace.services.vocabulary.importer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
