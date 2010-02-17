74765c7b7552fa88e10ee2a6b9da902a660c3250
Sanjay Dalal <sanjay.dalal@berkeley.edu>
2010-02-17 23:01:13 +0000

    CSPACE-1010 made length of userid in account consistent with that in users and users_roles test: account tests with security
    M    services/account/jaxb/src/main/resources/accounts_common.xsd
    M    services/account/pstore/src/main/resources/db/mysql/account.sql

--- services/account/pstore/src/main/resources/db/mysql/account.sql
+++ services/account/pstore/src/main/resources/db/mysql/account.sql
@@ -1,6 +1,6 @@
 alter table tenants drop foreign key FKAAE82D09C4F08FD6;
 drop table if exists accounts_common;
 drop table if exists tenants;
-create table accounts_common (csid varchar(255) not null, email longtext not null, mobile varchar(255), phone varchar(255), screen_name varchar(128) not null, status varchar(15) not null, userid longtext not null, primary key (csid), unique (screen_name));
+create table accounts_common (csid varchar(255) not null, email longtext not null, mobile varchar(255), phone varchar(255), screen_name varchar(128) not null, status varchar(15) not null, userid varchar(128) not null, primary key (csid), unique (screen_name));
 create table tenants (HJID bigint not null auto_increment, id varchar(255) not null, name varchar(255) not null, TENANT_ACCOUNTSCOMMON_CSID varchar(255), primary key (HJID));
 alter table tenants add index FKAAE82D09C4F08FD6 (TENANT_ACCOUNTSCOMMON_CSID), add constraint FKAAE82D09C4F08FD6 foreign key (TENANT_ACCOUNTSCOMMON_CSID) references accounts_common (csid);
