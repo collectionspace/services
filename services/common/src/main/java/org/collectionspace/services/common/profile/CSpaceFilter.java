4b4a7a19a3b8ea5ccf9a167bb02e4c71a75a450d
Aron Roberts <aron@socrates.berkeley.edu>
2010-09-27 07:05:54 +0000

    CSPACE-2950: Minor generalization of Profiler class. Temporary expedient in log4j config file and CSpace filter class to interleave CSV-style log messages with existing messages, to give Kasper a starting point. These CSV-style messages should instead be logged via a separate file appender.

--- services/common/src/main/java/org/collectionspace/services/common/profile/CSpaceFilter.java
+++ services/common/src/main/java/org/collectionspace/services/common/profile/CSpaceFilter.java
@@ -1,14 +1,4 @@
-/**	
- * CSpaceFilter.java
- *
- * {Purpose of This Class}
- *
- * {Other Notes Relating to This Class (Optional)}
- *
- * $LastChangedBy: $
- * $LastChangedRevision: $
- * $LastChangedDate: $
- *
+/**
  * This document is a part of the source code and related artifacts
  * for CollectionSpace, an open source collections management system
  * for museums and related institutions:
@@ -36,52 +26,92 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.ServletContext;
 
+import org.collectionspace.services.common.ServletTools;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * The Class CSpaceFilter.
+ * CSpaceFilter.java
+ *
+ * A filter that performs specified actions at the time
+ * each new request is received by the servlet container.
+ *
+ * This filter is currently used for recording performance
+ * metrics for requests to the CollectionSpace services.
+ *
+ * $LastChangedRevision: $
+ * $LastChangedDate: $
+ *
  */
 public class CSpaceFilter implements Filter {
-	
-	/** The filter config. */
-	FilterConfig filterConfig = null;
 
-	/* (non-Javadoc)
-	 * @see javax.servlet.Filter#destroy()
-	 */
-	@Override
-	public void destroy() {
-		// Empty method.
-	}
+    /** The filter config. */
+    FilterConfig filterConfig = null;
 
-	/* (non-Javadoc)
-	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
-	 */
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {		
-		if (request != null) {
-			HttpServletRequest httpRequest = (HttpServletRequest)request;
-			StringBuffer uri = new StringBuffer(httpRequest.getRequestURI());
-			uri.append(':');
-			uri.append(httpRequest.getMethod());
-			Profiler profiler = new Profiler(uri.toString(),
-					0);
-			profiler.start();
-			chain.doFilter(request, response);
-//			profiler.log(httpRequest.getRequestURI());
-			profiler.stop();
-		}
-	}
+    private final String CLASS_NAME = this.getClass().getSimpleName();
+    private Logger csvPerfLogger = LoggerFactory.getLogger("perf.collectionspace.csv");
 
-	/* (non-Javadoc)
-	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
-	 */
-	@Override
-	public void init(FilterConfig theFilterConfig) throws ServletException {
-		filterConfig = theFilterConfig;
-		if (filterConfig != null) {
-			// We can initialize using the init-params here which we defined in
-			// web.xml)
-		}
-	}
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#destroy()
+     */
+    @Override
+    public void destroy() {
+        // Empty method.
+    }
 
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        if (request != null) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+            // Instantiate the CollectionSpace services profiler.
+            StringBuffer uri = new StringBuffer(httpRequest.getRequestURI());
+            uri.append(':');
+            uri.append(httpRequest.getMethod());
+            Profiler profiler = new Profiler(uri.toString(), 0);
+
+            // Start timing.
+            profiler.start();
+
+            // Process the request.
+            chain.doFilter(request, response);
+
+            // Stop timing and log performance-related metrics.
+            profiler.stop();
+
+            String csvMsg =
+                    "," + System.currentTimeMillis()
+                    + "," + Thread.currentThread().getName()
+                    + "," + "app"
+                    + "," + "svc"
+                    + "," + CLASS_NAME
+                    + "," + httpRequest.getMethod()
+                    + "," + ServletTools.getURL(httpRequest)
+                    + "," + profiler.getElapsedTime();
+
+            // FIXME: Expedient change interleaves CSV-style log entries with entries
+            // in the original perf log.  These should be written to their own log file,
+            // with their own patternLayout.
+            profiler.log(csvMsg);
+            profiler.reset();
+            // csvPerfLogger.debug(csvMsg);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig theFilterConfig) throws ServletException {
+        filterConfig = theFilterConfig;
+        if (filterConfig != null) {
+            // We can initialize using the init-params here which we defined in
+            // web.xml)
+        }
+    }
 }
