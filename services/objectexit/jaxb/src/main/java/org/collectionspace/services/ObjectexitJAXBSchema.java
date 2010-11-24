20d6ec57caed652bb8021579ff29f52eb042a39b
Aron Roberts <aron@socrates.berkeley.edu>
2010-11-24 23:51:14 +0000

    CSPACE-3232: Made exitMethods field in Object Exit record repeatable in the services layer.

--- services/objectexit/jaxb/src/main/java/org/collectionspace/services/ObjectexitJAXBSchema.java
+++ services/objectexit/jaxb/src/main/java/org/collectionspace/services/ObjectexitJAXBSchema.java
@@ -7,6 +7,7 @@ public interface ObjectexitJAXBSchema {
     final static String OBJECT_EXIT_CURRENT_OWNER = "currentOwner";
     final static String OBJECT_EXIT_DEPOSITOR = "depositor";
     final static String OBJECT_EXIT_DATE = "exitDate";
+    final static String OBJECT_EXIT_METHODS = "exitMethods";
     final static String OBJECT_EXIT_METHOD = "exitMethod";
     final static String OBJECT_EXIT_NOTE = "exitNote";
     final static String OBJECT_EXIT_NUMBER = "exitNumber";
