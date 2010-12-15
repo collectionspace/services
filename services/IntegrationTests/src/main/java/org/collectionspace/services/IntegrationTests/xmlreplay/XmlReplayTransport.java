e08f1f1e39bdfdae4eeed20434c0d9e583301d27
Laramie Crocker <laramie@berkeley.edu>
2010-12-15 21:10:33 +0000

    CSPACE-3332

--- services/IntegrationTests/src/main/java/org/collectionspace/services/IntegrationTests/xmlreplay/XmlReplayTransport.java
+++ services/IntegrationTests/src/main/java/org/collectionspace/services/IntegrationTests/xmlreplay/XmlReplayTransport.java
@@ -39,7 +39,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import org.collectionspace.services.IntegrationTests.xmlreplay.ServiceResult;
+import org.collectionspace.services.common.Tools;
 
 /**
  *   @author Laramie Crocker
