08f8165becaf7cf35fd490a573c113c546a5e7f5
Aron Roberts <aron@socrates.berkeley.edu>
2011-10-11 19:30:02 +0000

    CSPACE-4468,CSPACE-4470: Added constants for subject and object doctype fields in relation records.

--- services/common/src/main/java/org/collectionspace/services/common/relation/RelationJAXBSchema.java
+++ services/common/src/main/java/org/collectionspace/services/common/relation/RelationJAXBSchema.java
@@ -60,10 +60,13 @@ public interface RelationJAXBSchema {
     final static String SUBJECT_URI = "subjectUri";
     final static String SUBJECT_CSID = "subjectCsid";
     final static String SUBJECT_REFNAME = "subjectRefName";
+    final static String SUBJECT_DOCTYPE = "subjectDocumentType";
 
     final static String OBJECT_URI = "objectUri";
     final static String OBJECT_CSID = "objectCsid";
     final static String OBJECT_REFNAME = "objectRefName";
+    final static String OBJECT_DOCTYPE = "objectDocumentType";
+
 }
 
 
