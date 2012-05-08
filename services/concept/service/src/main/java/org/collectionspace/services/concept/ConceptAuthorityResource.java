a4010052d9f80a022e3a68ea55746b36794029c5
Aron Roberts <aron@socrates.berkeley.edu>
2012-05-08 11:33:47 -0700

    CSPACE-5123,CSPACE-4844: Updated Concept schemas, validator handler to reflect repeatable term info group. Concept module builds if built without compiling tests; e.g. 'mvn install -Dmaven.test.skip=true'.

--- services/concept/service/src/main/java/org/collectionspace/services/concept/ConceptAuthorityResource.java
+++ services/concept/service/src/main/java/org/collectionspace/services/concept/ConceptAuthorityResource.java
@@ -1,75 +1,67 @@
 /**
- *  This document is a part of the source code and related artifacts
- *  for CollectionSpace, an open source collections management system
- *  for museums and related institutions:
-
- *  http://www.collectionspace.org
- *  http://wiki.collectionspace.org
-
- *  Copyright 2009 University of California at Berkeley
-
- *  Licensed under the Educational Community License (ECL), Version 2.0.
- *  You may not use this file except in compliance with this License.
-
- *  You may obtain a copy of the ECL 2.0 License at
-
- *  https://source.collectionspace.org/collection-space/LICENSE.txt
-
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * This document is a part of the source code and related artifacts for
+ * CollectionSpace, an open source collections management system for museums and
+ * related institutions:
+ *
+ * http://www.collectionspace.org http://wiki.collectionspace.org
+ *
+ * Copyright 2009 University of California at Berkeley
+ *
+ * Licensed under the Educational Community License (ECL), Version 2.0. You may
+ * not use this file except in compliance with this License.
+ *
+ * You may obtain a copy of the ECL 2.0 License at
+ *
+ * https://source.collectionspace.org/collection-space/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.collectionspace.services.concept;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import org.collectionspace.services.client.ConceptAuthorityClient;
-import org.collectionspace.services.client.PersonAuthorityClient;
 import org.collectionspace.services.common.vocabulary.AuthorityResource;
 import org.collectionspace.services.concept.nuxeo.ConceptDocumentModelHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
+/**
+ * ConceptAuthorityResource
+ *
+ * Handles, dispatches, and returns responses to RESTful requests related to
+ * Concept authority-related resources.
+ */
 @Path(ConceptAuthorityClient.SERVICE_PATH)
 @Consumes("application/xml")
 @Produces("application/xml")
-public class ConceptAuthorityResource 
-	extends AuthorityResource<ConceptauthoritiesCommon, 
-								ConceptDocumentModelHandler> {
+public class ConceptAuthorityResource
+        extends AuthorityResource<ConceptauthoritiesCommon, ConceptDocumentModelHandler> {
 
-    private final static String conceptAuthorityServiceName = "conceptauthorities";
-	private final static String CONCEPTAUTHORITIES_COMMON = "conceptauthorities_common";
-    
-    private final static String conceptServiceName = "concepts";
-	private final static String CONCEPTS_COMMON = "concepts_common";
-    
     final Logger logger = LoggerFactory.getLogger(ConceptAuthorityResource.class);
 
     public ConceptAuthorityResource() {
-		super(ConceptauthoritiesCommon.class, ConceptAuthorityResource.class,
-				CONCEPTAUTHORITIES_COMMON, CONCEPTS_COMMON);
+        super(ConceptauthoritiesCommon.class, ConceptAuthorityResource.class,
+                ConceptAuthorityClient.SERVICE_COMMON_PART_NAME, ConceptAuthorityClient.SERVICE_ITEM_COMMON_PART_NAME);
     }
 
     @Override
     public String getServiceName() {
-        return conceptAuthorityServiceName;
-    }
-
-    public String getItemServiceName() {
-        return conceptServiceName;
+        return ConceptAuthorityClient.SERVICE_NAME;
     }
 
     @Override
-    public Class<ConceptauthoritiesCommon> getCommonPartClass() {
-    	return ConceptauthoritiesCommon.class;
+    public String getItemServiceName() {
+        return ConceptAuthorityClient.SERVICE_ITEM_NAME;
     }
 
-	@Override
-	public String getItemTermInfoGroupXPathBase() {
+    @Override
+    public String getItemTermInfoGroupXPathBase() {
         return ConceptAuthorityClient.TERM_INFO_GROUP_XPATH_BASE;
-	}
+    }
 }
