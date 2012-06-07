0f956f32f88ab62641098bc3cb58032857bf1ed4
Aron Roberts <aron@socrates.berkeley.edu>
2012-06-07 14:18:59 -0700

    CSPACE-5271: Added second test case for generating a URI string via variable expansion, using a provided Map.

--- services/common/src/test/java/org/collectionspace/services/common/test/UriTemplateTest.java
+++ services/common/src/test/java/org/collectionspace/services/common/test/UriTemplateTest.java
@@ -21,14 +21,20 @@
  */
 package org.collectionspace.services.common.test;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.collectionspace.services.common.UriTemplate;
 import org.collectionspace.services.common.UriTemplateFactory;
+import org.collectionspace.services.common.api.Tools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UriTemplateTest {
+    
+    final static String EXAMPLE_SERVICE_NAME = "examples";
+    final static String CSID = "a87f6616-4146-4c17-a41a-048597cc12aa";
 
     private static final Logger logger = LoggerFactory.getLogger(UriTemplateTest.class);
 
@@ -38,13 +44,25 @@ public class UriTemplateTest {
     }
 
     @Test
-    public void testResourceUriTemplate() {
-        testBanner("testBuildResourceUri");
+    public void createResourceUriTemplate() {
+        testBanner("createResourceUriTemplate");
         UriTemplate resourceTemplate = UriTemplateFactory.getURITemplate(UriTemplateFactory.UriTemplateType.RESOURCE);
         Assert.assertNotNull(resourceTemplate, "Resource template is null.");
         logger.debug("Resource template URI path = " + resourceTemplate.toString());
         Assert.assertNotNull(resourceTemplate.toString(), "Resource template URI path is null.");
         Assert.assertEquals(resourceTemplate.toString(), UriTemplateFactory.RESOURCE_TEMPLATE_PATTERN,
                 "Resource template URI path doesn't match expected path.");
+    }
+    
+    @Test (dependsOnMethods = {"createResourceUriTemplate"})
+    public void buildResourceUri() {
+        testBanner("buildResourceUri");
+        UriTemplate resourceTemplate = UriTemplateFactory.getURITemplate(UriTemplateFactory.UriTemplateType.RESOURCE);
+        Map<String,String> resourceUriVars = new HashMap<String,String>();
+        resourceUriVars.put("servicename", EXAMPLE_SERVICE_NAME);
+        resourceUriVars.put("identifier", CSID);
+        String uriStr = resourceTemplate.buildUri(resourceUriVars);
+        Assert.assertFalse(Tools.isBlank(uriStr), "Generated URI string is null or blank.");
+        logger.debug("Generated URI string = " + uriStr);
     }
 }
