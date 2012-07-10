576153641903cca5f1f6e7a52c3ee9334fbd5992
Aron Roberts <aron@socrates.berkeley.edu>
2012-07-10 16:56:27 -0700

    CSPACE-5359: findDocs now accepts an ORDER BY statement, while still defaulting to returning documents in descending order by updatedAt timestamps. When updating display names in authority references, we now obtain documents in ascending order by createdAt timestamps, which should be static for existing records.

--- services/common/src/main/java/org/collectionspace/services/common/vocabulary/RefNameServiceUtils.java
+++ services/common/src/main/java/org/collectionspace/services/common/vocabulary/RefNameServiceUtils.java
@@ -291,9 +291,8 @@ public class RefNameServiceUtils {
         return refNameServiceTypes;
     }
     // Seems like a good value - no real data to set this well.
-    // private static final int N_OBJS_TO_UPDATE_PER_LOOP = 100;
-    // FIXME: This value is set to 3 during debugging; needs to be set higher during production - ADR 2012-07-10
-    private static final int N_OBJS_TO_UPDATE_PER_LOOP = 3;
+    // Note: can set this value lower; e.g. to 3 during debugging; - ADR 2012-07-10
+    private static final int N_OBJS_TO_UPDATE_PER_LOOP = 100;
 
     public static int updateAuthorityRefDocs(
             ServiceContext<PoxPayloadIn, PoxPayloadOut> ctx,
@@ -404,7 +403,7 @@ public class RefNameServiceUtils {
         // Now we have to issue the search
         RepositoryJavaClientImpl nuxeoRepoClient = (RepositoryJavaClientImpl) repoClient;
         DocumentWrapper<DocumentModelList> docListWrapper = nuxeoRepoClient.findDocs(ctx, repoSession,
-                docTypes, query, pageSize, pageNum, computeTotal);
+                docTypes, query, orderByClause, pageSize, pageNum, computeTotal);
         // Now we gather the info for each document into the list and return
         DocumentModelList docList = docListWrapper.getWrappedObject();
         return docList;
