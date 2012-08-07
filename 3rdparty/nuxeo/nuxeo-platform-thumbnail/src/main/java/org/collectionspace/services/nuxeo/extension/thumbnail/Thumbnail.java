9f4e2257b965b30f51f48a1c53dd92668cf39c48
Richard Millet <remillet@berkeley.edu>
2012-08-07 16:02:54 -0700

    CSPACE-5437: Adding more Nuxeo thumbnail generation files.

---
+++ 3rdparty/nuxeo/nuxeo-platform-thumbnail/src/main/java/org/collectionspace/services/nuxeo/extension/thumbnail/Thumbnail.java
@@ -0,0 +1,25 @@
+package org.collectionspace.services.nuxeo.extension.thumbnail;
+
+import java.io.Serializable;
+
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.DocumentModel;
+
+public class Thumbnail {
+	private DocumentModel docModel;
+	
+	public Thumbnail(DocumentModel doc) {
+		docModel = doc;
+	}
+	
+	public String getDigest() throws ClientException {
+		String result = null;
+		
+		if (docModel != null) {
+			result = (String) docModel.getProperty(ThumbnailConstants.THUMBNAIL_SCHEMA_NAME,
+					ThumbnailConstants.THUMBNAIL_DIGEST_PROPERTY_NAME);
+		}
+		
+		return result;
+	}	
+}
