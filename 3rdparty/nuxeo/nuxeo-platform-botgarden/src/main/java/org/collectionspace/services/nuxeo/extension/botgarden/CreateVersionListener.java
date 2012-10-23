421a97e9522041af142309cf129bad04c024274a
Ray Lee <rhlee@berkeley.edu>
2012-10-23 12:58:41 -0700

    UCBG-42: Normalize indentation.

--- 3rdparty/nuxeo/nuxeo-platform-botgarden/src/main/java/org/collectionspace/services/nuxeo/extension/botgarden/CreateVersionListener.java
+++ 3rdparty/nuxeo/nuxeo-platform-botgarden/src/main/java/org/collectionspace/services/nuxeo/extension/botgarden/CreateVersionListener.java
@@ -16,49 +16,49 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 
 public class CreateVersionListener implements EventListener {
 	public static final String SKIP_PROPERTY = "CreateVersionListener.SKIP";
-	
+
 	final Log logger = LogFactory.getLog(CreateVersionListener.class);
 
-    public void handleEvent(Event event) throws ClientException {
-        EventContext ec = event.getContext();
-        
-        if (ec instanceof DocumentEventContext) {
-            DocumentEventContext context = (DocumentEventContext) ec;
-            
-            if (ec.hasProperty(SKIP_PROPERTY) && ((Boolean) ec.getProperty(SKIP_PROPERTY))) {
-            	logger.debug("Skipping create version");
-            }
-            else {
-	            DocumentModel doc = context.getSourceDocument();
-	
-	            logger.debug("docType=" + doc.getType());
-	
-	            if (doc.getType().startsWith(MovementConstants.NUXEO_DOCTYPE) && 
-	            		!doc.isVersion() && 
-	            		!doc.isProxy() &&
-	            		!doc.getCurrentLifeCycleState().equals(MovementConstants.DELETED_STATE)) {
-	            	String csid = doc.getName();
-	            	
-	            	// Temporarily change the csid, so that the version we create will have a unique csid
-	            	String newCsid = UUID.randomUUID().toString();
-	            	context.getCoreSession().move(doc.getRef(), doc.getParentRef(), newCsid);
-	            	
-	            	// Version the document
-	            	DocumentRef versionRef = doc.checkIn(VersioningOption.MINOR, null);        	
-	            	DocumentModel versionDoc = context.getCoreSession().getDocument(versionRef);
-	 
-	            	logger.debug("created version: id=" + versionDoc.getId() + " csid=" + versionDoc.getName());
-	            	           	
-	            	// Delete the version, so search doesn't find it
-	           		context.getCoreSession().followTransition(versionRef, "delete");
-	            	
-	            	// Check out the document, so it can be modified
-	            	doc.checkOut();
-	            	
-	            	// Reset the csid
-	            	context.getCoreSession().move(doc.getRef(), doc.getParentRef(), csid);
-	            }
-            }
-        }
-    }
+	public void handleEvent(Event event) throws ClientException {
+		EventContext ec = event.getContext();
+
+		if (ec instanceof DocumentEventContext) {
+			DocumentEventContext context = (DocumentEventContext) ec;
+
+			if (ec.hasProperty(SKIP_PROPERTY) && ((Boolean) ec.getProperty(SKIP_PROPERTY))) {
+				logger.debug("Skipping create version");
+			}
+			else {
+				DocumentModel doc = context.getSourceDocument();
+
+				logger.debug("docType=" + doc.getType());
+
+				if (doc.getType().startsWith(MovementConstants.NUXEO_DOCTYPE) && 
+						!doc.isVersion() && 
+						!doc.isProxy() &&
+						!doc.getCurrentLifeCycleState().equals(MovementConstants.DELETED_STATE)) {
+					String csid = doc.getName();
+
+					// Temporarily change the csid, so that the version we create will have a unique csid
+					String newCsid = UUID.randomUUID().toString();
+					context.getCoreSession().move(doc.getRef(), doc.getParentRef(), newCsid);
+
+					// Version the document
+					DocumentRef versionRef = doc.checkIn(VersioningOption.MINOR, null);        	
+					DocumentModel versionDoc = context.getCoreSession().getDocument(versionRef);
+
+					logger.debug("created version: id=" + versionDoc.getId() + " csid=" + versionDoc.getName());
+
+					// Delete the version, so search doesn't find it
+					context.getCoreSession().followTransition(versionRef, "delete");
+
+					// Check out the document, so it can be modified
+					doc.checkOut();
+
+					// Reset the csid
+					context.getCoreSession().move(doc.getRef(), doc.getParentRef(), csid);
+				}
+			}
+		}
+	}
 }
