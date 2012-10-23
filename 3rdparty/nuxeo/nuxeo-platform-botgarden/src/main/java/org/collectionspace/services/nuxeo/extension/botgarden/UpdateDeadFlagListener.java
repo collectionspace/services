e0df456c408995da2a7b0a85c1e9a90baa69e542
Ray Lee <rhlee@berkeley.edu>
2012-10-23 13:01:11 -0700

    UCBG-42: Normalize indentation.

--- 3rdparty/nuxeo/nuxeo-platform-botgarden/src/main/java/org/collectionspace/services/nuxeo/extension/botgarden/UpdateDeadFlagListener.java
+++ 3rdparty/nuxeo/nuxeo-platform-botgarden/src/main/java/org/collectionspace/services/nuxeo/extension/botgarden/UpdateDeadFlagListener.java
@@ -20,83 +20,83 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 public class UpdateDeadFlagListener implements EventListener {
 	final Log logger = LogFactory.getLog(UpdateDeadFlagListener.class);
 
-    /* 
-     * Set the dead flag and dead date on collectionobjects related to a new or modified movement record.
-     */
-    public void handleEvent(Event event) throws ClientException {
-        EventContext ec = event.getContext();
+	/* 
+	 * Set the dead flag and dead date on collectionobjects related to a new or modified movement record.
+	 */
+	public void handleEvent(Event event) throws ClientException {
+		EventContext ec = event.getContext();
 
-        if (ec instanceof DocumentEventContext) {
-            DocumentEventContext context = (DocumentEventContext) ec;
-            DocumentModel doc = context.getSourceDocument();
-            
-            logger.debug("docType=" + doc.getType());
-            
-            if (event.getName().equals(DocumentEventTypes.DOCUMENT_CREATED)) {
-            	/*
-            	 * Handle the case where a new movement is created with action code revive, and then related
-            	 * to a collectionobject. The movement won't have any relations at the time it's created,
-            	 * so we need to capture the creation of the relation. 
-            	 */
-            	if (doc.getType().equals(RelationConstants.NUXEO_DOCTYPE) &&
-	            		!doc.isVersion() && 
-	            		!doc.isProxy()) {
-            		String subjectDocType = (String) doc.getProperty(RelationConstants.SUBJECT_DOCTYPE_SCHEMA_NAME, RelationConstants.SUBJECT_DOCTYPE_FIELD_NAME);
-            		String objectDocType = (String) doc.getProperty(RelationConstants.OBJECT_DOCTYPE_SCHEMA_NAME, RelationConstants.OBJECT_DOCTYPE_FIELD_NAME);;
-            		
-            		logger.debug("subjectDocType=" + subjectDocType + " objectDocType=" + objectDocType);
-            		
-            		if (subjectDocType.equals(MovementConstants.NUXEO_DOCTYPE) && objectDocType.equals(CollectionObjectConstants.NUXEO_DOCTYPE)) {
-            			String movementCsid = (String) doc.getProperty(RelationConstants.SUBJECT_CSID_SCHEMA_NAME, RelationConstants.SUBJECT_CSID_FIELD_NAME);
-               			String collectionObjectCsid = (String) doc.getProperty(RelationConstants.OBJECT_CSID_SCHEMA_NAME, RelationConstants.OBJECT_CSID_FIELD_NAME);
-               		    
-	            		try {
-	            			InvocationResults results = createUpdater().updateDeadFlag(collectionObjectCsid, movementCsid);
-	            			
-	            			logger.debug("updateDeadFlag complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
-	            		} catch (Exception e) {
-	            			logger.error(e.getMessage(), e);
-	            		}            		
-	            	}
-            	}
-            }
-            else {
-            	/*
-            	 * Handle document modification. If the modified document was a movement record, and 
-            	 * its action code is dead or revived, update the dead flag. We don't actually have to
-            	 * check the action code here, since it will be checked inside UpdateDeadFlagBatchJob.updateRelatedDeadFlags,
-            	 * but it is an optimization.
-            	 */
-            	if (doc.getType().startsWith(MovementConstants.NUXEO_DOCTYPE) &&
-	            		!doc.isVersion() && 
-	            		!doc.isProxy() && 
-	            		!doc.getCurrentLifeCycleState().equals(MovementConstants.DELETED_STATE)) {
-	            	String actionCode = (String) doc.getProperty(MovementConstants.ACTION_CODE_SCHEMA_NAME, MovementConstants.ACTION_CODE_FIELD_NAME);           	
-	            
-	            	logger.debug("actionCode=" + actionCode);
-	            	
-	            	if (actionCode != null && (actionCode.equals(MovementConstants.DEAD_ACTION_CODE) || actionCode.equals(MovementConstants.REVIVED_ACTION_CODE))) {
-	            		String movementCsid = doc.getName();
-	            				
-	            		try {
-	            			InvocationResults results = createUpdater().updateRelatedDeadFlags(movementCsid);
-	            			
-	            			logger.debug("updateRelatedDeadFlags complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
-	            		} catch (Exception e) {
-	            			logger.error(e.getMessage(), e);
-	            		}
-	            	}
-            	}
-            }
-        }
-    }
-    
-    private UpdateDeadFlagBatchJob createUpdater() {
+		if (ec instanceof DocumentEventContext) {
+			DocumentEventContext context = (DocumentEventContext) ec;
+			DocumentModel doc = context.getSourceDocument();
+
+			logger.debug("docType=" + doc.getType());
+
+			if (event.getName().equals(DocumentEventTypes.DOCUMENT_CREATED)) {
+				/*
+				 * Handle the case where a new movement is created with action code revive, and then related
+				 * to a collectionobject. The movement won't have any relations at the time it's created,
+				 * so we need to capture the creation of the relation. 
+				 */
+				if (doc.getType().equals(RelationConstants.NUXEO_DOCTYPE) &&
+						!doc.isVersion() && 
+						!doc.isProxy()) {
+					String subjectDocType = (String) doc.getProperty(RelationConstants.SUBJECT_DOCTYPE_SCHEMA_NAME, RelationConstants.SUBJECT_DOCTYPE_FIELD_NAME);
+					String objectDocType = (String) doc.getProperty(RelationConstants.OBJECT_DOCTYPE_SCHEMA_NAME, RelationConstants.OBJECT_DOCTYPE_FIELD_NAME);;
+
+					logger.debug("subjectDocType=" + subjectDocType + " objectDocType=" + objectDocType);
+
+					if (subjectDocType.equals(MovementConstants.NUXEO_DOCTYPE) && objectDocType.equals(CollectionObjectConstants.NUXEO_DOCTYPE)) {
+						String movementCsid = (String) doc.getProperty(RelationConstants.SUBJECT_CSID_SCHEMA_NAME, RelationConstants.SUBJECT_CSID_FIELD_NAME);
+						String collectionObjectCsid = (String) doc.getProperty(RelationConstants.OBJECT_CSID_SCHEMA_NAME, RelationConstants.OBJECT_CSID_FIELD_NAME);
+
+						try {
+							InvocationResults results = createUpdater().updateDeadFlag(collectionObjectCsid, movementCsid);
+
+							logger.debug("updateDeadFlag complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
+						} catch (Exception e) {
+							logger.error(e.getMessage(), e);
+						}            		
+					}
+				}
+			}
+			else {
+				/*
+				 * Handle document modification. If the modified document was a movement record, and 
+				 * its action code is dead or revived, update the dead flag. We don't actually have to
+				 * check the action code here, since it will be checked inside UpdateDeadFlagBatchJob.updateRelatedDeadFlags,
+				 * but it is an optimization.
+				 */
+				if (doc.getType().startsWith(MovementConstants.NUXEO_DOCTYPE) &&
+						!doc.isVersion() && 
+						!doc.isProxy() && 
+						!doc.getCurrentLifeCycleState().equals(MovementConstants.DELETED_STATE)) {
+					String actionCode = (String) doc.getProperty(MovementConstants.ACTION_CODE_SCHEMA_NAME, MovementConstants.ACTION_CODE_FIELD_NAME);           	
+
+					logger.debug("actionCode=" + actionCode);
+
+					if (actionCode != null && (actionCode.equals(MovementConstants.DEAD_ACTION_CODE) || actionCode.equals(MovementConstants.REVIVED_ACTION_CODE))) {
+						String movementCsid = doc.getName();
+
+						try {
+							InvocationResults results = createUpdater().updateRelatedDeadFlags(movementCsid);
+
+							logger.debug("updateRelatedDeadFlags complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
+						} catch (Exception e) {
+							logger.error(e.getMessage(), e);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private UpdateDeadFlagBatchJob createUpdater() {
 		ResourceMap resourceMap = ResteasyProviderFactory.getContextData(ResourceMap.class);
-		
+
 		UpdateDeadFlagBatchJob updater = new UpdateDeadFlagBatchJob();
 		updater.setResourceMap(resourceMap);
 
 		return updater;
-    }
+	}
 }
--- 3rdparty/nuxeo/nuxeo-platform-botgarden/src/main/java/org/collectionspace/services/nuxeo/extension/botgarden/UpdateLocationListener.java
+++ 3rdparty/nuxeo/nuxeo-platform-botgarden/src/main/java/org/collectionspace/services/nuxeo/extension/botgarden/UpdateLocationListener.java
@@ -15,63 +15,63 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 
 public class UpdateLocationListener implements EventListener {
 	final Log logger = LogFactory.getLog(UpdateLocationListener.class);
-	 
-    /* 
-     * Set the currentLocation and previousLocation fields in a Current Location record
-     * to appropriate values.
-     * 
-     * <ul>
-     * <li>If the plant is dead, set currentLocation to none</li>
-     * <li>Set the previousLocation field to the previous value of the currentLocation field</li>
-     * </ui>
-     */
-    public void handleEvent(Event event) throws ClientException {
-        EventContext ec = event.getContext();
 
-        if (ec instanceof DocumentEventContext) {
-            DocumentEventContext context = (DocumentEventContext) ec;
-            DocumentModel doc = context.getSourceDocument();
+	/* 
+	 * Set the currentLocation and previousLocation fields in a Current Location record
+	 * to appropriate values.
+	 * 
+	 * <ul>
+	 * <li>If the plant is dead, set currentLocation to none</li>
+	 * <li>Set the previousLocation field to the previous value of the currentLocation field</li>
+	 * </ui>
+	 */
+	public void handleEvent(Event event) throws ClientException {
+		EventContext ec = event.getContext();
 
-            if (doc.getType().startsWith(NUXEO_DOCTYPE) && 
-            		!doc.isVersion() && 
-            		!doc.isProxy() && 
-            		!doc.getCurrentLifeCycleState().equals(DELETED_STATE)) {
-            	String actionCode = (String) doc.getProperty(ACTION_CODE_SCHEMA_NAME, ACTION_CODE_FIELD_NAME);
-            	
-            	logger.debug("actionCode=" + actionCode);
+		if (ec instanceof DocumentEventContext) {
+			DocumentEventContext context = (DocumentEventContext) ec;
+			DocumentModel doc = context.getSourceDocument();
 
-            	if (event.getName().equals(DocumentEventTypes.DOCUMENT_CREATED)) {
-            		/*
-            		 * Special case for a document that is created with an action code of dead.
-            		 * In this case, we'll set the currentLocation to none, and the previousLocation to
-            		 * the current value of currentLocation, since there isn't a previous value. To do 
-            		 * this, we can simply save the document, which will cause the beforeDocumentModification
-            		 * event to fire, taking us into the other branch of this code, with the current document
-            		 * becoming the previous document.
-            		 */
-	            	if (actionCode != null && actionCode.equals(DEAD_ACTION_CODE)) {
-	             		context.getCoreSession().saveDocument(doc);
-	             		
-	             		/*
-	             		 *  The saveDocument call will have caused the document to be versioned via documentModified,
-	             		 *  so we can skip the versioning that would normally happen on documentCreated.
-	             		 */
-	            		ec.setProperty(CreateVersionListener.SKIP_PROPERTY, true);
-	            	}
-            	}
-            	else {	            	
-	            	if (actionCode != null && actionCode.equals(DEAD_ACTION_CODE)) {
-	             		doc.setProperty(CURRENT_LOCATION_SCHEMA_NAME, CURRENT_LOCATION_FIELD_NAME, NONE_LOCATION);
-	            	}
-	            	            	
-	            	DocumentModel previousDoc = (DocumentModel) context.getProperty(CoreEventConstants.PREVIOUS_DOCUMENT_MODEL);	            	
-	        		String previousLocation = (String) previousDoc.getProperty(CURRENT_LOCATION_SCHEMA_NAME, CURRENT_LOCATION_FIELD_NAME);
-	        		
-	        		logger.debug("previousLocation=" + previousLocation);
-	        		
-	        		doc.setProperty(PREVIOUS_LOCATION_SCHEMA_NAME, PREVIOUS_LOCATION_FIELD_NAME, previousLocation);
-            	}
-            }
-        }
-    }
+			if (doc.getType().startsWith(NUXEO_DOCTYPE) && 
+					!doc.isVersion() && 
+					!doc.isProxy() && 
+					!doc.getCurrentLifeCycleState().equals(DELETED_STATE)) {
+				String actionCode = (String) doc.getProperty(ACTION_CODE_SCHEMA_NAME, ACTION_CODE_FIELD_NAME);
+
+				logger.debug("actionCode=" + actionCode);
+
+				if (event.getName().equals(DocumentEventTypes.DOCUMENT_CREATED)) {
+					/*
+					 * Special case for a document that is created with an action code of dead.
+					 * In this case, we'll set the currentLocation to none, and the previousLocation to
+					 * the current value of currentLocation, since there isn't a previous value. To do 
+					 * this, we can simply save the document, which will cause the beforeDocumentModification
+					 * event to fire, taking us into the other branch of this code, with the current document
+					 * becoming the previous document.
+					 */
+					if (actionCode != null && actionCode.equals(DEAD_ACTION_CODE)) {
+						context.getCoreSession().saveDocument(doc);
+
+						/*
+						 *  The saveDocument call will have caused the document to be versioned via documentModified,
+						 *  so we can skip the versioning that would normally happen on documentCreated.
+						 */
+						ec.setProperty(CreateVersionListener.SKIP_PROPERTY, true);
+					}
+				}
+				else {	            	
+					if (actionCode != null && actionCode.equals(DEAD_ACTION_CODE)) {
+						doc.setProperty(CURRENT_LOCATION_SCHEMA_NAME, CURRENT_LOCATION_FIELD_NAME, NONE_LOCATION);
+					}
+
+					DocumentModel previousDoc = (DocumentModel) context.getProperty(CoreEventConstants.PREVIOUS_DOCUMENT_MODEL);	            	
+					String previousLocation = (String) previousDoc.getProperty(CURRENT_LOCATION_SCHEMA_NAME, CURRENT_LOCATION_FIELD_NAME);
+
+					logger.debug("previousLocation=" + previousLocation);
+
+					doc.setProperty(PREVIOUS_LOCATION_SCHEMA_NAME, PREVIOUS_LOCATION_FIELD_NAME, previousLocation);
+				}
+			}
+		}
+	}
 }
