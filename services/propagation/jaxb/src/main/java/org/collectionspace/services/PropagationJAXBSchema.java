9ee0f78d27a6723a293ebb736bce2f79353dd135
amy wieliczka <amywieliczka@berkeley.edu>
2012-10-25 11:39:10 -0700

    UCBG-58: Prop Date and Activity Date changed to structured dates

--- services/propagation/jaxb/src/main/java/org/collectionspace/services/PropagationJAXBSchema.java
+++ services/propagation/jaxb/src/main/java/org/collectionspace/services/PropagationJAXBSchema.java
@@ -5,7 +5,7 @@ package org.collectionspace.services;
 
 public interface PropagationJAXBSchema {
     final static String PROP_NUMBER = "propNumber";
-    final static String PROP_DATE = "propDate";
+    // final static String PROP_DATE = "propDate";
     final static String PROP_REASON = "propReason";
     final static String PROP_TYPE = "propType";
     final static String PROP_COMMENTS = "propComments";
