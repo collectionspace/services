f48e4bdbfe274ffdbfc94b291a20f02a9a70197a
Ray Lee <rhlee@berkeley.edu>
2012-12-04 11:29:33 -0800

    UCBG-104: Bug fixes to TaxonFormatter.

--- services/common-api/src/main/java/org/collectionspace/services/common/api/TaxonFormatter.java
+++ services/common-api/src/main/java/org/collectionspace/services/common/api/TaxonFormatter.java
@@ -1,5 +1,8 @@
 package org.collectionspace.services.common.api;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.StringUtils;
 import org.gbif.api.model.checklistbank.ParsedName;
 import org.gbif.nameparser.NameParser;
@@ -9,7 +12,8 @@ import org.slf4j.LoggerFactory;
 
 public class TaxonFormatter {
     private static final Logger logger = LoggerFactory.getLogger(TaxonFormatter.class);
-
+    private static final Pattern HYBRID_PATTERN = Pattern.compile("^(.*?)( [×xX] )(.*)$");
+    
     private NameParser nameParser;
     
     public TaxonFormatter() {
@@ -17,9 +21,32 @@ public class TaxonFormatter {
     }
     
 	public String format(String name) {
-		try {
-			ParsedName parsedName = nameParser.parse(name);
+		if (StringUtils.isBlank(name)) {
+			return name;
+		}
+
+		Matcher hybridMatcher = HYBRID_PATTERN.matcher(name);
+		
+		if (hybridMatcher.matches()) {
+			String parentName1 = hybridMatcher.group(1);
+			String separator = hybridMatcher.group(2);
+			String parentName2 = hybridMatcher.group(3);
 			
+			logger.debug("hybrid formula: parentName1=" + parentName1 + " parentName2=" + parentName2);
+			
+			return (format(parentName1) + separator + format(parentName2));
+		}
+		
+		ParsedName parsedName = null;
+
+		try {
+			parsedName = nameParser.parse(name);
+		}
+		catch (UnparsableException e) {
+			logger.error("error parsing name: name=" + name + " message=" + e.getMessage());
+		}
+
+		if (parsedName != null) {
 			String genusOrAbove = parsedName.getGenusOrAbove();
 			String specificEpithet = parsedName.getSpecificEpithet();
 			String infraSpecificEpithet = parsedName.getInfraSpecificEpithet();
@@ -36,16 +63,13 @@ public class TaxonFormatter {
 			
 			if (StringUtils.isNotBlank(infraSpecificEpithet)) {
 				name = italicize(name, infraSpecificEpithet);
-			}			
-		}
-		catch (UnparsableException e) {
-			logger.error("error parsing name: name=" + name + " message=" + e.getMessage());
+			}	
 		}
 
 		return name;
 	}
 	
 	private String italicize(String string, String substring) {
-		return string.replaceAll(substring, "<span style=\"font-style: italic\">" + substring + "</span>");
+		return Pattern.compile(substring, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.LITERAL).matcher(string).replaceAll("<span style=\"font-style: italic\">$0</span>");
 	}
 }
