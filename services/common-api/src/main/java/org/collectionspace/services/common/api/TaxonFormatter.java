a730eb46018bd8f67aaf631e040e0a5b3027818d
Ray Lee <rhlee@berkeley.edu>
2012-12-05 17:22:02 -0800

    UCBG-104: Update TaxonFormatter to handle some corner cases in SAGE data.

--- services/common-api/src/main/java/org/collectionspace/services/common/api/TaxonFormatter.java
+++ services/common-api/src/main/java/org/collectionspace/services/common/api/TaxonFormatter.java
@@ -12,7 +12,11 @@ import org.slf4j.LoggerFactory;
 
 public class TaxonFormatter {
     private static final Logger logger = LoggerFactory.getLogger(TaxonFormatter.class);
-    private static final Pattern HYBRID_PATTERN = Pattern.compile("^(.*?)( [×xX] )(.*)$");
+    private static final Pattern HYBRID_FORMULA_PATTERN = Pattern.compile("^(.*?)(\\s[×xX]\\s)(.*)$");
+    private static final Pattern BROKEN_HYBRID_FORMULA_PATTERN = Pattern.compile("^×\\s*|\\s*×$");
+    private static final Pattern ADJACENT_ITALIC_TAG_PATTERN = Pattern.compile("</i>(\\s*)<i>");
+    private static final Pattern STARTS_WITH_INFRASPECIFIC_RANK_PATTERN = Pattern.compile("^\\s*(var|subsp|cv|aff)\\.");
+    private static final Pattern SUBSPECIES_WITH_QUALIFIER_PATTERN = Pattern.compile("(\\s|^)(subsp\\.\\s+)(section|subsection|ser\\.|sser\\.)(\\s|$)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
     
     private NameParser nameParser;
     
@@ -25,7 +29,7 @@ public class TaxonFormatter {
 			return name;
 		}
 
-		Matcher hybridMatcher = HYBRID_PATTERN.matcher(name);
+		Matcher hybridMatcher = HYBRID_FORMULA_PATTERN.matcher(name);
 		
 		if (hybridMatcher.matches()) {
 			String parentName1 = hybridMatcher.group(1);
@@ -37,13 +41,48 @@ public class TaxonFormatter {
 			return (format(parentName1) + separator + format(parentName2));
 		}
 		
+		String normalizedName = name;
+
+		if (BROKEN_HYBRID_FORMULA_PATTERN.matcher(normalizedName).find()) {
+			logger.info("broken hybrid: name=" + name + " normalizedName=" + normalizedName);
+			
+			normalizedName = BROKEN_HYBRID_FORMULA_PATTERN.matcher(normalizedName).replaceAll("");			
+		}
+
+		if (SUBSPECIES_WITH_QUALIFIER_PATTERN.matcher(normalizedName).find()) {
+			logger.info("qualified subspecies: name=" + name + " normalizedName=" + normalizedName);
+			
+			normalizedName = SUBSPECIES_WITH_QUALIFIER_PATTERN.matcher(normalizedName).replaceAll("$1$2$4");			
+		}
+
+		if (STARTS_WITH_INFRASPECIFIC_RANK_PATTERN.matcher(normalizedName).find()) {
+			/*
+			 * There are some non-standard taxon names in SAGE data, where there is an infraspecific rank/epithet, but no genus/species, e.g.
+			 *     subsp. occidentalis (J.T. Howell) C.B. Wolf 
+			 * 
+			 * Since the GBIF parser can't handle this, we'll temporarily prefix an arbitrary genus and species for parsing purposes.
+			 */
+			logger.info("name starts with infraspecific rank: name=" + name + " normalizedName=" + normalizedName);
+			
+			normalizedName = "Tempgenus tempspecies " + normalizedName;
+		}
+		
 		ParsedName parsedName = null;
 
 		try {
-			parsedName = nameParser.parse(name);
+			parsedName = nameParser.parse(normalizedName);
 		}
 		catch (UnparsableException e) {
-			logger.error("error parsing name: name=" + name + " message=" + e.getMessage());
+			logger.info("Unparsable name, trying with a temp genus: name=" + name + " normalizedName=" + normalizedName);
+			
+			normalizedName = "Tempgenus " + normalizedName;
+			
+			try {
+				parsedName = nameParser.parse(normalizedName);
+			}
+			catch (UnparsableException ex) {		
+				logger.error("error parsing name: name=" + name + " normalizedName=" + normalizedName + " message=" + e.getMessage());
+			}
 		}
 
 		if (parsedName != null) {
@@ -63,13 +102,22 @@ public class TaxonFormatter {
 			
 			if (StringUtils.isNotBlank(infraSpecificEpithet)) {
 				name = italicize(name, infraSpecificEpithet);
-			}	
+			}
+			
+			name = compressTags(name);
 		}
 
 		return name;
 	}
 	
 	private String italicize(String string, String substring) {
-		return Pattern.compile(substring, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.LITERAL).matcher(string).replaceAll("<span style=\"font-style: italic\">$0</span>");
+		//return Pattern.compile(substring, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.LITERAL).matcher(string).replaceAll("<span style=\"font-style: italic\">$0</span>");
+		return Pattern.compile("(\\s|^)(" + Pattern.quote(substring) + ")(\\s|$)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(string).replaceAll("$1<i>$2</i>$3");
+	}
+	
+	private String compressTags(String html) {
+		html = ADJACENT_ITALIC_TAG_PATTERN.matcher(html).replaceAll("$1");
+		
+		return html;
 	}
 }
