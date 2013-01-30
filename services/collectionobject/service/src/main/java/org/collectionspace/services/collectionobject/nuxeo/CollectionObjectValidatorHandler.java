873874c00fbce8393e038089e9d1481387360d31
Ray Lee <rhlee@berkeley.edu>
2013-01-29 16:56:53 -0800

    PAHMA-683: Move PAHMA collectionobject validation into a custom validatorHandler.

--- services/collectionobject/service/src/main/java/org/collectionspace/services/collectionobject/nuxeo/CollectionObjectValidatorHandler.java
+++ services/collectionobject/service/src/main/java/org/collectionspace/services/collectionobject/nuxeo/CollectionObjectValidatorHandler.java
@@ -95,9 +95,8 @@ public class CollectionObjectValidatorHandler extends ValidatorHandlerImpl<PoxPa
 	protected void handleUpdate()
 			throws InvalidDocumentException {
     	try {
-	        // PAHMA-473: Disable non-empty objectNumber requirement, so that updates don't need to retrieve the current objectNumber.
-            //CollectionobjectsCommon co = (CollectionobjectsCommon) getCommonPart();
-            //validateCollectionobjectsCommon(co);                        
+            CollectionobjectsCommon co = (CollectionobjectsCommon) getCommonPart();
+            validateCollectionobjectsCommon(co);                        
     	} catch (AssertionError e) {
     		if (logger.isErrorEnabled() == true) {
     			logger.error(e.getMessage(), e);
