985a78a79146ec146445f04d13d5c3f5dd00ae17
Ray Lee <rhlee@berkeley.edu>
2013-03-12 18:55:47 -0700

    UCBG-220: Update load_botgarden_id_generators.sql to work when idgenerators already exist. Also remove some commented out generators.

--- services/common/src/main/resources/db/postgresql/load_botgarden_id_generators.sql
+++ services/common/src/main/resources/db/postgresql/load_botgarden_id_generators.sql
@@ -45,7 +45,7 @@
 
 INSERT INTO id_generators
     (csid, displayname, description, priority, last_generated_id, id_generator_state)
-  VALUES 
+  SELECT 
     ('ed3c8578-cf01-4a05-b7fc-8e49b96273c6',
      'UC Accession Number',
      'Identifies individual collection objects formally
@@ -65,14 +65,20 @@ without parts.',
       <currentValue>-1</currentValue>
     </org.collectionspace.services.id.NumericIDGeneratorPart>
   </parts>
-</org.collectionspace.services.id.SettableIDGenerator>');
+</org.collectionspace.services.id.SettableIDGenerator>')
+WHERE 'ed3c8578-cf01-4a05-b7fc-8e49b96273c6' NOT IN
+      (
+      SELECT  csid
+      FROM    id_generators
+      );
+
 
 
 -- JEPS_ACCESSION_NUMBER
 
 INSERT INTO id_generators
     (csid, displayname, description, priority, last_generated_id, id_generator_state)
-  VALUES 
+  SELECT 
     ('d9eca382-23b7-47aa-8d9b-d7ff88f49e2b',
      'JEPS Accession Number',
      'Identifies individual collection objects formally
@@ -92,64 +98,20 @@ without parts.',
       <currentValue>-1</currentValue>
     </org.collectionspace.services.id.NumericIDGeneratorPart>
   </parts>
-</org.collectionspace.services.id.SettableIDGenerator>');
-
--- UC_LOANS_OUT_NUMBER
-
--- INSERT INTO id_generators
---     (csid, displayname, description, priority, last_generated_id, id_generator_state)
---   VALUES 
---     ('9e1526e8-a0a3-4e1c-9ced-060ddc02450d',
---      'UC Loan Out Number',
---      'Identifies activities in which collection objects are
--- loaned out of the institution.',
---      '9',
---      '',
--- '<org.collectionspace.services.id.SettableIDGenerator>
---   <parts>
---     <org.collectionspace.services.id.StringIDGeneratorPart>
---       <initialValue>UC</initialValue>
---       <currentValue>UC</currentValue>
---     </org.collectionspace.services.id.StringIDGeneratorPart>
---     <org.collectionspace.services.id.NumericIDGeneratorPart>
---       <maxLength>9</maxLength>
---       <initialValue>1</initialValue>
---       <currentValue>-1</currentValue>
---     </org.collectionspace.services.id.NumericIDGeneratorPart>
---   </parts>
--- </org.collectionspace.services.id.SettableIDGenerator>');
+</org.collectionspace.services.id.SettableIDGenerator>')
+WHERE 'd9eca382-23b7-47aa-8d9b-d7ff88f49e2b' NOT IN
+      (
+      SELECT  csid
+      FROM    id_generators
+      );
 
 
--- JEPS_LOANS_OUT_NUMBER
-
--- INSERT INTO id_generators
---     (csid, displayname, description, priority, last_generated_id, id_generator_state)
---   VALUES 
---     ('84e1bd24-c0cc-4873-9f2a-ef89fc1d4299',
---      'JEPS Loan Out Number',
---      'Identifies activities in which collection objects are
--- loaned out of the institution.',
---      '9',
---      '',
--- '<org.collectionspace.services.id.SettableIDGenerator>
---   <parts>
---     <org.collectionspace.services.id.StringIDGeneratorPart>
---       <initialValue>JEPS</initialValue>
---       <currentValue>JEPS</currentValue>
---     </org.collectionspace.services.id.StringIDGeneratorPart>
---     <org.collectionspace.services.id.NumericIDGeneratorPart>
---       <maxLength>9</maxLength>
---       <initialValue>1</initialValue>
---       <currentValue>-1</currentValue>
---     </org.collectionspace.services.id.NumericIDGeneratorPart>
---   </parts>
--- </org.collectionspace.services.id.SettableIDGenerator>');
 
 -- VOUCHER_NUMBER
 
 INSERT INTO id_generators
     (csid, displayname, description, priority, last_generated_id, id_generator_state)
-  VALUES 
+  SELECT 
     ('940710aa-3df4-4bd4-8d6f-9bf6a92b35cc',
      'Voucher Collection Number',
      'Identifies Botanical Garden vouchers.',
@@ -174,13 +136,19 @@ INSERT INTO id_generators
       <currentValue>-1</currentValue>
     </org.collectionspace.services.id.NumericIDGeneratorPart>
   </parts>
-</org.collectionspace.services.id.SettableIDGenerator>');
+</org.collectionspace.services.id.SettableIDGenerator>')
+WHERE '940710aa-3df4-4bd4-8d6f-9bf6a92b35cc' NOT IN
+      (
+      SELECT  csid
+      FROM    id_generators
+      );
+
 
 -- PROP_NUMBER
 
 INSERT INTO id_generators
     (csid, displayname, description, priority, last_generated_id, id_generator_state)
-  VALUES 
+  SELECT 
     ('81cf5a56-d43d-49e9-ac11-61cf4b0923d4',
      'Propagation Number',
      'Identifies activities in which accessions are
@@ -199,4 +167,9 @@ propagated.',
       <currentValue>-1</currentValue>
     </org.collectionspace.services.id.NumericIDGeneratorPart>
   </parts>
-</org.collectionspace.services.id.SettableIDGenerator>');
+</org.collectionspace.services.id.SettableIDGenerator>')
+WHERE '81cf5a56-d43d-49e9-ac11-61cf4b0923d4' NOT IN
+      (
+      SELECT  csid
+      FROM    id_generators
+      );
