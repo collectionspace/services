7b6313d8ddefa9cb5e0b651ddb3d599166fdf464
Ray Lee <rhlee@berkeley.edu>
2013-10-02 19:36:25 -0700

    BAMPFA-20: Add theme vocab to set_cinefiles_vocabulary_csids.sql, and tenant-qualify the updates.

--- services/common/src/main/resources/db/postgresql/set_cinefiles_vocabulary_csids.sql
+++ services/common/src/main/resources/db/postgresql/set_cinefiles_vocabulary_csids.sql
@@ -1,24 +1,25 @@
 -- Set vocabulary csids to known values. This script should be run after recreating a database and initializing authorities.
 
 -- Citation
-update hierarchy set name='0fada64d-eb2c-442d-b951' where id = (select id from citationauthorities_common where shortidentifier='citation');
+update hierarchy set name='0fada64d-eb2c-442d-b951' where id = (select common.id from citationauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='citation');
 
 -- Concept
-update hierarchy set name='e80e2b37-5e34-475a-b9d8' where id = (select id from conceptauthorities_common where shortidentifier='concept');
-update hierarchy set name='74571044-8b19-4d55-970d' where id = (select id from conceptauthorities_common where shortidentifier='genre');
+update hierarchy set name='e80e2b37-5e34-475a-b9d8' where id = (select common.id from conceptauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='concept');
+update hierarchy set name='74571044-8b19-4d55-970d' where id = (select common.id from conceptauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='genre');
+update hierarchy set name='46cf0ea2-adfa-4694-8687' where id = (select common.id from conceptauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='theme');
 
 -- Location
-update hierarchy set name='84a9a735-27f1-4b74-9955' where id = (select id from locationauthorities_common where shortidentifier='location');
+update hierarchy set name='84a9a735-27f1-4b74-9955' where id = (select common.id from locationauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='location');
 
 -- Organization
-update hierarchy set name='79c7ae8c-c4dc-46b4-ad00' where id = (select id from orgauthorities_common where shortidentifier='committee');
-update hierarchy set name='5a1317ec-6246-4737-9d92' where id = (select id from orgauthorities_common where shortidentifier='organization');
+update hierarchy set name='79c7ae8c-c4dc-46b4-ad00' where id = (select common.id from orgauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='committee');
+update hierarchy set name='5a1317ec-6246-4737-9d92' where id = (select common.id from orgauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='organization');
 
 -- Person
-update hierarchy set name='ae43b316-d4ff-475f-921c' where id = (select id from personauthorities_common where shortidentifier='person');
+update hierarchy set name='ae43b316-d4ff-475f-921c' where id = (select common.id from personauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='person');
 
 -- Place
-update hierarchy set name='bba3b2f9-ca14-4d5c-821c' where id = (select id from placeauthorities_common where shortidentifier='place');
+update hierarchy set name='bba3b2f9-ca14-4d5c-821c' where id = (select common.id from placeauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='place');
 
 -- Work
-update hierarchy set name='d28dfc96-da03-4b0b-9ae1' where id = (select id from workauthorities_common where shortidentifier='work');
+update hierarchy set name='d28dfc96-da03-4b0b-9ae1' where id = (select common.id from workauthorities_common common inner join collectionspace_core cc on common.id=cc.id where cc.tenantid=50 and shortidentifier='work');
