1fd1c5a78d3e5ace02d832db94d06127a0170761
Ray Lee <rhlee@berkeley.edu>
2014-02-20 16:09:12 -0800

    UCJEPS-497: Move custom collectionobject validator into a new class.

---
+++ services/collectionobject/service/src/main/java/org/collectionspace/services/collectionobject/nuxeo/UCJepsCollectionObjectValidatorHandler.java
@@ -0,0 +1,18 @@
+package org.collectionspace.services.collectionobject.nuxeo;
+
+import org.collectionspace.services.common.document.InvalidDocumentException;
+
+public class UCJepsCollectionObjectValidatorHandler extends CollectionObjectValidatorHandler {
+
+	@Override
+	protected void handleUpdate() throws InvalidDocumentException {
+		// Allow an empty object number.
+
+	}
+	
+	@Override
+	protected void handleCreate() throws InvalidDocumentException {
+		// Allow an empty object number.
+
+	}
+}
