7ffa6a52c07410ae99e8565944596000b60fde36
Ray Lee <rhlee@berkeley.edu>
2014-03-03 17:48:45 -0800

    BAMPFA-138: Add PFA Stills id generator.

--- services/common/src/main/resources/db/postgresql/load_cinefiles_id_generators.sql
+++ services/common/src/main/resources/db/postgresql/load_cinefiles_id_generators.sql
@@ -1,10 +1,10 @@
--- DOCUMENT NUMBER
+-- CINEFILES DOCUMENT NUMBER
 
 INSERT INTO id_generators
     (csid, displayname, description, priority, last_generated_id, id_generator_state)
   SELECT 
      '5abe7f10-cfeb-4c00-b6b5-bdf621692f0f',
-     'Document Number',
+     'CineFiles Document Number',
      'Identifies CineFiles Documents.',
      '9',
      '',
@@ -18,6 +18,35 @@ INSERT INTO id_generators
   </parts>
 </org.collectionspace.services.id.SettableIDGenerator>'
   WHERE '5abe7f10-cfeb-4c00-b6b5-bdf621692f0f' NOT IN
+        (
+        SELECT  csid
+        FROM    id_generators
+        );
+
+-- PFA STILLS NUMBER
+
+INSERT INTO id_generators
+    (csid, displayname, description, priority, last_generated_id, id_generator_state)
+  SELECT 
+     'cfbbe386-ee42-45e4-9759-df4d4d940496',
+     'PFA Stills Number',
+     'Identifies PFA Stills.',
+     '9',
+     '',
+'<org.collectionspace.services.id.SettableIDGenerator>
+  <parts>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>PS</initialValue>
+      <currentValue>PS</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.NumericIDGeneratorPart>
+      <maxLength>7</maxLength>
+      <initialValue>1</initialValue>
+      <currentValue>-1</currentValue>
+    </org.collectionspace.services.id.NumericIDGeneratorPart>
+  </parts>
+</org.collectionspace.services.id.SettableIDGenerator>'
+  WHERE 'cfbbe386-ee42-45e4-9759-df4d4d940496' NOT IN
         (
         SELECT  csid
         FROM    id_generators
