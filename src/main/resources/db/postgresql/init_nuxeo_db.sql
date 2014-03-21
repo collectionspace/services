684bd11ef42ea47da17e822ec1ecd9ae5dcc73e7
Aron Roberts <aron@socrates.berkeley.edu>
2014-03-21 13:31:05 -0700

    CSPACE-6349: Additional tweaks to comments in Nuxeo databases initialization script.

--- src/main/resources/db/postgresql/init_nuxeo_db.sql
+++ src/main/resources/db/postgresql/init_nuxeo_db.sql
@@ -1,12 +1,9 @@
+
 -- init_nuxeo_db.sql
 
 -- This file holds SQL statements which can be run to initialize
 -- the Nuxeo-managed databases. A copy of this file is deployed to the 
 -- CollectionSpace server folder during a build of the Services layer.
-
--- Beginning with CollectionSpace version 4.1, the contents of the deployed
--- copy of this file are no longer static. Rather, that file's contents are
--- now written during each CollectionSpace system startup, via Services layer code.
 
 -- You can manually invoke the initialization commands in that file
 -- by entering the command "ant create_nuxeo_db" from the top of the
@@ -14,7 +11,12 @@
 -- the Nuxeo-managed databases and their associated user roles, and
 -- will irrevocably DESTROY your data contained in those databases.)
 
+-- Beginning with CollectionSpace version 4.1, the contents of the deployed
+-- copy of this file are no longer static. Rather, that file's contents are
+-- now written during each CollectionSpace system startup, via Services
+-- layer code. (Code-written contents appear at the top of that file.)
+
 -- Any additional initialization statements not already written to that
 -- file during startup can be added here, following this comment. You'll
--- need to deploy this file to the CollectionSpace server folder, to have
--- those additional statements take effect:
+-- need to deploy this file to the CollectionSpace server folder, via the
+-- command "ant deploy", to have those additional statements take effect:
