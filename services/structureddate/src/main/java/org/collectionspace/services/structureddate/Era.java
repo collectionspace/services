8e354d868f8790899bf1799bccb83716a864abdf
Ray Lee <rhlee@berkeley.edu>
2014-06-04 16:20:04 -0700

    NOJIRA: Change Era values to bampfa refnames.

--- services/structureddate/src/main/java/org/collectionspace/services/structureddate/Era.java
+++ services/structureddate/src/main/java/org/collectionspace/services/structureddate/Era.java
@@ -1,8 +1,8 @@
 package org.collectionspace.services.structureddate;
 
 public enum Era {
-	BCE ("urn:cspace:cinefiles.cspace.berkeley.edu:vocabularies:name(dateera):item:name(bce)'BCE'"),
-	CE  ("urn:cspace:cinefiles.cspace.berkeley.edu:vocabularies:name(dateera):item:name(ce)'CE'");
+	BCE ("urn:cspace:bampfa.cspace.berkeley.edu:vocabularies:name(dateera):item:name(bce)'BC'"),
+	CE  ("urn:cspace:bampfa.cspace.berkeley.edu:vocabularies:name(dateera):item:name(ce)'AD'");
 	
 	private final String value;
 	
