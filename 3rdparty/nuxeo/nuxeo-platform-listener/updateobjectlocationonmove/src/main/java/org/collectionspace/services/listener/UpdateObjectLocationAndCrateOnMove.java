c9c76af58faadaed16de5823630b985792cde11f
Ray Lee <rhlee@berkeley.edu>
2014-09-12 18:14:51 -0700

    BAMPFA-258: Enable UpdateObjectLocationAndCrateOnMove listener for bampfa tenant.

--- 3rdparty/nuxeo/nuxeo-platform-listener/updateobjectlocationonmove/src/main/java/org/collectionspace/services/listener/UpdateObjectLocationAndCrateOnMove.java
+++ 3rdparty/nuxeo/nuxeo-platform-listener/updateobjectlocationonmove/src/main/java/org/collectionspace/services/listener/UpdateObjectLocationAndCrateOnMove.java
@@ -16,6 +16,8 @@ public class UpdateObjectLocationAndCrateOnMove extends UpdateObjectLocationOnMo
     // FIXME: Get values below from external constants
     private final static String COLLECTIONOBJECTS_ANTHROPOLOGY_SCHEMA = "collectionobjects_anthropology";
     private final static String MOVEMENTS_ANTHROPOLOGY_SCHEMA = "movements_anthropology";
+    private final static String COLLECTIONOBJECTS_BAMPFA_SCHEMA = "collectionobjects_bampfa";
+    private final static String MOVEMENTS_BAMPFA_SCHEMA = "movements_bampfa";
     private final static String CRATE_PROPERTY = "crate";
     private final static String COMPUTED_CRATE_PROPERTY = "computedCrate";
 
@@ -33,7 +35,7 @@ public class UpdateObjectLocationAndCrateOnMove extends UpdateObjectLocationOnMo
         
         // Get the current crate value from the Movement (the "new" value)
         String crateRefName =
-                (String) movementDocModel.getProperty(MOVEMENTS_ANTHROPOLOGY_SCHEMA, CRATE_PROPERTY);
+                (String) movementDocModel.getProperty(MOVEMENTS_BAMPFA_SCHEMA, CRATE_PROPERTY);
 
         // Check that the value returned, which is expected to be a
         // reference (refName) to an authority term:
@@ -53,7 +55,7 @@ public class UpdateObjectLocationAndCrateOnMove extends UpdateObjectLocationOnMo
         // Get the computed crate value of the CollectionObject
         // (the "existing" value)
         String existingCrateRefName =
-                (String) collectionObjectDocModel.getProperty(COLLECTIONOBJECTS_ANTHROPOLOGY_SCHEMA,
+                (String) collectionObjectDocModel.getProperty(COLLECTIONOBJECTS_BAMPFA_SCHEMA,
                 COMPUTED_CRATE_PROPERTY);
         if (logger.isTraceEnabled()) {
             logger.trace("Existing crate refName=" + existingCrateRefName);
@@ -62,7 +64,7 @@ public class UpdateObjectLocationAndCrateOnMove extends UpdateObjectLocationOnMo
         // If the new value is blank, any non-blank existing value should always
         // be overwritten ('nulled out') with a blank value.
         if (Tools.isBlank(crateRefName) && Tools.notBlank(existingCrateRefName)) {
-            collectionObjectDocModel.setProperty(COLLECTIONOBJECTS_ANTHROPOLOGY_SCHEMA,
+            collectionObjectDocModel.setProperty(COLLECTIONOBJECTS_BAMPFA_SCHEMA,
                     COMPUTED_CRATE_PROPERTY, (Serializable) null);
             // Otherwise, if the new value is not blank, and
             // * the existing value is blank, or
@@ -75,7 +77,7 @@ public class UpdateObjectLocationAndCrateOnMove extends UpdateObjectLocationOnMo
             }
             // ... update the existing value in the CollectionObject with the
             // new value from the Movement.
-            collectionObjectDocModel.setProperty(COLLECTIONOBJECTS_ANTHROPOLOGY_SCHEMA,
+            collectionObjectDocModel.setProperty(COLLECTIONOBJECTS_BAMPFA_SCHEMA,
                     COMPUTED_CRATE_PROPERTY, crateRefName);
         } else {
             if (logger.isTraceEnabled()) {
