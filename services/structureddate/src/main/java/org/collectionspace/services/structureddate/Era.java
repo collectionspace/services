307670b9e06a3d5108634504109bbf1479d95247
Ray Lee <rhlee@berkeley.edu>
2015-05-20 19:18:31 -0700

    PAHMA-1224: Correct structured date enums.

--- services/structureddate/src/main/java/org/collectionspace/services/structureddate/Era.java
+++ services/structureddate/src/main/java/org/collectionspace/services/structureddate/Era.java
@@ -1,8 +1,8 @@
 package org.collectionspace.services.structureddate;
 
 public enum Era {
-	BCE ("urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(dateera):item:name(bce)'BC/BCE'"),
-	CE  ("urn:cspace:pahma.cspace.berkeley.edu:vocabularies:name(dateera):item:name(ce)'AD/CE'");
+	BCE ("bce"),
+	CE  ("ce");
 	
 	private final String value;
 	
