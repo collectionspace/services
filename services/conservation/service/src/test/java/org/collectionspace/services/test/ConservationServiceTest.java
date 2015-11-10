71617e4bf4079923e3580c6d9104ff1c30fc3dfa
Ray Lee <rhlee@berkeley.edu>
2015-11-09 17:19:19 -0800

    CSPACE-6902: Add conservation procedure.

---
+++ services/conservation/service/src/test/java/org/collectionspace/services/test/ConservationServiceTest.java
@@ -0,0 +1,13 @@
+package org.collectionspace.services.test;
+
+//import org.collectionspace.services.conservation.Conservation;
+//import org.collectionspace.services.conservation.ConservationList;
+
+/**
+ * Placeholder for server-side testing of Conservation service code.
+ * 
+ * @version $Revision$
+ */
+public class ConservationServiceTest {
+	//empty
+}
