efbbfbfbf995a68a8a247a2100b9a89c6ad6ed33
Ray Lee <rhlee@berkeley.edu>
2016-01-20 23:16:34 -0800

    PAHMA-1455: Write reports to temp files instead of memory.

--- services/report/service/src/main/java/org/collectionspace/services/report/nuxeo/ReportDocumentModelHandler.java
+++ services/report/service/src/main/java/org/collectionspace/services/report/nuxeo/ReportDocumentModelHandler.java
@@ -28,7 +28,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -277,8 +279,9 @@ public class ReportDocumentModelHandler extends DocHandlerBase<ReportsCommon> {
 			// export report to pdf and build a response with the bytes
 			//JasperExportManager.exportReportToPdf(jasperprint);
 			
-			// Report will be to a byte output array.
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			// Output to a temp file, since large reports may overrun the heap.
+			File tempOutputFile = Files.createTempFile("report-", null).toFile();
+			FileOutputStream tempOutputStream = new FileOutputStream(tempOutputFile);
 
 			JRExporter exporter = null;
 			// Strip extension from report filename.
@@ -338,9 +341,12 @@ public class ReportDocumentModelHandler extends DocHandlerBase<ReportsCommon> {
 			JasperPrint jasperPrint = JasperFillManager.fillReport(fileStream, params,conn);
 				
 			exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
-			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, baos);
+			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, tempOutputStream);
 			exporter.exportReport();
-			result = new ByteArrayInputStream(baos.toByteArray());
+			
+			tempOutputStream.close();
+			
+			result = new FileInputStream(tempOutputFile);
 	
 	       	return result;
         } catch (SQLException sqle) {
