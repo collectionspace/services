53f95e5f9b8a6da04f9cb79641a389947b093fe9
Richard Millet <remillet@gmail.com>
2019-05-15 12:34:41 -0700

    DRYD-647: Merging in some UCB changes.

--- 3rdparty/nuxeo/nuxeo-platform-listener/botgarden/src/main/java/org/collectionspace/services/listener/botgarden/UpdateDeadFlagListener.java
+++ 3rdparty/nuxeo/nuxeo-platform-listener/botgarden/src/main/java/org/collectionspace/services/listener/botgarden/UpdateDeadFlagListener.java
@@ -2,14 +2,20 @@ package org.collectionspace.services.listener.botgarden;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.collectionspace.services.batch.BatchResource;
 import org.collectionspace.services.batch.nuxeo.UpdateDeadFlagBatchJob;
+import org.collectionspace.services.client.BatchClient;
+import org.collectionspace.services.client.PoxPayloadIn;
+import org.collectionspace.services.client.PoxPayloadOut;
 import org.collectionspace.services.client.workflow.WorkflowClient;
 import org.collectionspace.services.collectionobject.nuxeo.CollectionObjectConstants;
 import org.collectionspace.services.common.ResourceMap;
+import org.collectionspace.services.common.context.ServiceContext;
 import org.collectionspace.services.common.invocable.InvocationResults;
 import org.collectionspace.services.common.relation.nuxeo.RelationConstants;
 import org.collectionspace.services.movement.nuxeo.MovementBotGardenConstants;
 import org.collectionspace.services.movement.nuxeo.MovementConstants;
+import org.collectionspace.services.nuxeo.client.java.CoreSessionWrapper;
 import org.collectionspace.services.nuxeo.listener.AbstractCSEventListenerImpl;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -21,7 +27,7 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 public class UpdateDeadFlagListener extends AbstractCSEventListenerImpl {
 	final Log logger = LogFactory.getLog(UpdateDeadFlagListener.class);
 
-	/* 
+	/*
 	 * Set the dead flag and dead date on collectionobjects related to a new or modified movement record.
 	 */
 	@Override
@@ -38,10 +44,10 @@ public class UpdateDeadFlagListener extends AbstractCSEventListenerImpl {
 				/*
 				 * Handle the case where a new movement is created with action code revive, and then related
 				 * to a collectionobject. The movement won't have any relations at the time it's created,
-				 * so we need to capture the creation of the relation. 
+				 * so we need to capture the creation of the relation.
 				 */
 				if (doc.getType().equals(RelationConstants.NUXEO_DOCTYPE) &&
-						!doc.isVersion() && 
+						!doc.isVersion() &&
 						!doc.isProxy()) {
 					String subjectDocType = (String) doc.getProperty(RelationConstants.SUBJECT_DOCTYPE_SCHEMA_NAME, RelationConstants.SUBJECT_DOCTYPE_FIELD_NAME);
 					String objectDocType = (String) doc.getProperty(RelationConstants.OBJECT_DOCTYPE_SCHEMA_NAME, RelationConstants.OBJECT_DOCTYPE_FIELD_NAME);;
@@ -53,27 +59,27 @@ public class UpdateDeadFlagListener extends AbstractCSEventListenerImpl {
 						String collectionObjectCsid = (String) doc.getProperty(RelationConstants.OBJECT_CSID_SCHEMA_NAME, RelationConstants.OBJECT_CSID_FIELD_NAME);
 
 						try {
-							InvocationResults results = createUpdater().updateDeadFlag(collectionObjectCsid, movementCsid);
+							InvocationResults results = createUpdater(context).updateDeadFlag(collectionObjectCsid, movementCsid);
 
 							logger.debug("updateDeadFlag complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
 						} catch (Exception e) {
 							logger.error(e.getMessage(), e);
-						}            		
+						}
 					}
 				}
 			}
 			else {
 				/*
-				 * Handle document modification. If the modified document was a movement record, and 
+				 * Handle document modification. If the modified document was a movement record, and
 				 * its action code is dead or revived, update the dead flag. We don't actually have to
 				 * check the action code here, since it will be checked inside UpdateDeadFlagBatchJob.updateRelatedDeadFlags,
 				 * but it is an optimization.
 				 */
 				if (doc.getType().startsWith(MovementConstants.NUXEO_DOCTYPE) &&
-						!doc.isVersion() && 
-						!doc.isProxy() && 
+						!doc.isVersion() &&
+						!doc.isProxy() &&
 						!doc.getCurrentLifeCycleState().equals(WorkflowClient.WORKFLOWSTATE_DELETED)) {
-					String actionCode = (String) doc.getProperty(MovementBotGardenConstants.ACTION_CODE_SCHEMA_NAME, MovementBotGardenConstants.ACTION_CODE_FIELD_NAME);           	
+					String actionCode = (String) doc.getProperty(MovementBotGardenConstants.ACTION_CODE_SCHEMA_NAME, MovementBotGardenConstants.ACTION_CODE_FIELD_NAME);
 
 					logger.debug("actionCode=" + actionCode);
 
@@ -81,7 +87,7 @@ public class UpdateDeadFlagListener extends AbstractCSEventListenerImpl {
 						String movementCsid = doc.getName();
 
 						try {
-							InvocationResults results = createUpdater().updateRelatedDeadFlags(movementCsid);
+							InvocationResults results = createUpdater(context).updateRelatedDeadFlags(movementCsid);
 
 							logger.debug("updateRelatedDeadFlags complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
 						} catch (Exception e) {
@@ -93,10 +99,15 @@ public class UpdateDeadFlagListener extends AbstractCSEventListenerImpl {
 		}
 	}
 
-	private UpdateDeadFlagBatchJob createUpdater() {
+	private UpdateDeadFlagBatchJob createUpdater(DocumentEventContext context) throws Exception {
 		ResourceMap resourceMap = ResteasyProviderFactory.getContextData(ResourceMap.class);
+		BatchResource batchResource = (BatchResource) resourceMap.get(BatchClient.SERVICE_NAME);
+		ServiceContext<PoxPayloadIn, PoxPayloadOut> serviceContext = batchResource.createServiceContext(batchResource.getServiceName());
+
+		serviceContext.setCurrentRepositorySession(new CoreSessionWrapper(context.getCoreSession()));
 
 		UpdateDeadFlagBatchJob updater = new UpdateDeadFlagBatchJob();
+		updater.setServiceContext(serviceContext);
 		updater.setResourceMap(resourceMap);
 
 		return updater;
--- 3rdparty/nuxeo/nuxeo-platform-listener/naturalhistory/src/main/java/org/collectionspace/services/listener/naturalhistory/UpdateFormattedDisplayNameListener.java
+++ 3rdparty/nuxeo/nuxeo-platform-listener/naturalhistory/src/main/java/org/collectionspace/services/listener/naturalhistory/UpdateFormattedDisplayNameListener.java
@@ -13,6 +13,7 @@ import org.collectionspace.services.common.api.TaxonFormatter;
 import org.collectionspace.services.nuxeo.listener.AbstractCSEventListenerImpl;
 import org.collectionspace.services.taxonomy.nuxeo.TaxonBotGardenConstants;
 import org.collectionspace.services.taxonomy.nuxeo.TaxonConstants;
+import org.collectionspace.services.taxonomy.nuxeo.TaxonomyAuthorityConstants;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.event.CoreEventConstants;
 import org.nuxeo.ecm.core.api.event.DocumentEventTypes;
@@ -26,12 +27,12 @@ public class UpdateFormattedDisplayNameListener extends AbstractCSEventListenerI
 	private static final String[] DISPLAY_NAME_PATH_ELEMENTS = TaxonConstants.DISPLAY_NAME_FIELD_NAME.split("/");
 	private static final String TERM_GROUP_LIST_FIELD_NAME = DISPLAY_NAME_PATH_ELEMENTS[0];
 	private static final String DISPLAY_NAME_FIELD_NAME = DISPLAY_NAME_PATH_ELEMENTS[2];
-	
+
 	private static final String[] FORMATTED_DISPLAY_NAME_PATH_ELEMENTS = TaxonConstants.FORMATTED_DISPLAY_NAME_FIELD_NAME.split("/");
 	private static final String FORMATTED_DISPLAY_NAME_FIELD_NAME = FORMATTED_DISPLAY_NAME_PATH_ELEMENTS[2];
 
 	final Log logger = LogFactory.getLog(UpdateFormattedDisplayNameListener.class);
-	
+
 	@Override
 	public void handleEvent(Event event) {
 		EventContext ec = event.getContext();
@@ -39,35 +40,37 @@ public class UpdateFormattedDisplayNameListener extends AbstractCSEventListenerI
 		if (isRegistered(event) && ec instanceof DocumentEventContext) {
 			DocumentEventContext context = (DocumentEventContext) ec;
 			DocumentModel doc = context.getSourceDocument();
+			String docType = doc.getType();
 
-			logger.debug("docType=" + doc.getType());
-			
-			if (doc.getType().startsWith(TaxonConstants.NUXEO_DOCTYPE) && 
-					!doc.isVersion() && 
-					!doc.isProxy() && 
+			logger.debug("docType=" + docType);
+
+			if (docType.startsWith(TaxonConstants.NUXEO_DOCTYPE) &&
+					!docType.startsWith(TaxonomyAuthorityConstants.NUXEO_DOCTYPE) &&
+					!doc.isVersion() &&
+					!doc.isProxy() &&
 					!doc.getCurrentLifeCycleState().equals(WorkflowClient.WORKFLOWSTATE_DELETED)) {
-				
+
 				String refName = (String) doc.getProperty(TaxonConstants.REFNAME_SCHEMA_NAME, TaxonConstants.REFNAME_FIELD_NAME);
 				RefName.AuthorityItem item = RefName.AuthorityItem.parse(refName);
 				String parentShortId = item.getParentShortIdentifier();
-				
+
 				logger.debug("parentShortId=" + parentShortId);
-				
+
 				if (!parentShortId.equals(TaxonBotGardenConstants.COMMON_VOCABULARY_SHORTID)) {
 					if (event.getName().equals(DocumentEventTypes.DOCUMENT_CREATED)) {
 						// Save the document, to get the BEFORE_DOC_UPDATE branch to run.
 						doc.getCoreSession().saveDocument(doc);
 					}
 					else if (event.getName().equals(DocumentEventTypes.BEFORE_DOC_UPDATE)) {
-						DocumentModel previousDoc = (DocumentModel) context.getProperty(CoreEventConstants.PREVIOUS_DOCUMENT_MODEL);	            	
-	
+						DocumentModel previousDoc = (DocumentModel) context.getProperty(CoreEventConstants.PREVIOUS_DOCUMENT_MODEL);
+
 						updateFormattedDisplayNames(doc, previousDoc);
 					}
 				}
 			}
 		}
 	}
-	
+
 	private void updateFormattedDisplayNames(DocumentModel doc, DocumentModel previousDoc) {
 		//Set<String> previousDisplayNames = getDisplayNames(previousDoc);
 		TaxonFormatter formatter = new TaxonFormatter();
@@ -76,24 +79,24 @@ public class UpdateFormattedDisplayNameListener extends AbstractCSEventListenerI
 		for (Map<String, Object> termGroup : termGroupList) {
 			String displayName = (String) termGroup.get(DISPLAY_NAME_FIELD_NAME);
 			String formattedDisplayName = (String) termGroup.get(FORMATTED_DISPLAY_NAME_FIELD_NAME);
-			
+
 			if (StringUtils.isBlank(formattedDisplayName)) {
 				formattedDisplayName = "";
-				
+
 				if (StringUtils.isNotBlank(displayName)) {
 					formattedDisplayName = formatter.format(displayName);
 				}
-	
+
 				termGroup.put(FORMATTED_DISPLAY_NAME_FIELD_NAME, formattedDisplayName);
 			}
 		}
-		
+
 		Map<String, Object> updateMap = new HashMap<String, Object>();
 		updateMap.put(TERM_GROUP_LIST_FIELD_NAME, termGroupList);
-		
+
 		doc.setProperties(TaxonConstants.DISPLAY_NAME_SCHEMA_NAME, updateMap);
 	}
-	
+
 	/*
 	private Set<String> getDisplayNames(DocumentModel doc) throws ClientException {
 		Set<String> displayNames = new HashSet<String>();
@@ -101,12 +104,12 @@ public class UpdateFormattedDisplayNameListener extends AbstractCSEventListenerI
 
 		for (Map<String, Object> termGroup : termGroupList) {
 			String displayName = (String) termGroup.get(DISPLAY_NAME_FIELD_NAME);
-			
+
 			if (displayName != null) {
 				displayNames.add(displayName);
 			}
 		}
-		
+
 		return displayNames;
 	}
 	*/
