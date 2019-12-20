9004999a461bba438d921d0f41a077ff7006b546
Richard Millet <remillet@gmail.com>
2019-12-19 20:46:26 -0800

    CC-904: Updated BAMPFA specific listeners to use correct base classes.

--- 3rdparty/nuxeo/nuxeo-platform-listener/bampfa/src/main/java/org/collectionspace/services/listener/UpdateArtistDisplayNameListener.java
+++ 3rdparty/nuxeo/nuxeo-platform-listener/bampfa/src/main/java/org/collectionspace/services/listener/UpdateArtistDisplayNameListener.java
@@ -22,14 +22,16 @@ import org.collectionspace.services.common.ResourceMap;
 import org.collectionspace.services.common.context.ServiceContext;
 import org.collectionspace.services.common.invocable.InvocationResults;
 import org.collectionspace.services.nuxeo.client.java.CoreSessionWrapper;
+import org.collectionspace.services.nuxeo.listener.AbstractCSEventSyncListenerImpl;
+
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.event.CoreEventConstants;
 import org.nuxeo.ecm.core.api.event.DocumentEventTypes;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
-import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.event.EventListener;
 
@@ -39,7 +41,7 @@ import org.nuxeo.ecm.core.event.EventListener;
  *
  * @author Cesar Villalobos
  */
-public class UpdateArtistDisplayNameListener implements EventListener {
+public class UpdateArtistDisplayNameListener extends AbstractCSEventSyncListenerImpl {
 
     private final static Log logger = LogFactory.getLog(UpdateArtistDisplayNameListener.class);
     private final static String NO_FURTHER_PROCESSING_MESSAGE =
@@ -55,79 +57,6 @@ public class UpdateArtistDisplayNameListener implements EventListener {
     public static final String PREVIOUS_DISPLAYNAME_PROPERTY = "UpdateArtistDisplayNameListener.previousName";
 
     public static final String DISPLAYNAME_FIELD = "personTermGroupList";
-
-    @Override
-    public void handleEvent(Event event) throws ClientException {
-        logger.trace("In handleEvent in update nationalities listener.");
-
-        EventContext eventContext = event.getContext();
-        if (eventContext == null || !(eventContext instanceof DocumentEventContext)) {
-            return;
-        }
-
-        DocumentEventContext docEventContext = (DocumentEventContext) eventContext;
-        DocumentModel docModel = docEventContext.getSourceDocument();
-        String docType = docModel.getType();
-
-        // Check if the event involves a person authority
-        if (documentMatchesType(docModel, PERSON_DOCTYPE) &&
-					!documentMatchesType(docModel, "Personauthority") &&
-					!docModel.isVersion() &&
-					!docModel.isProxy() &&
-					!docModel.getCurrentLifeCycleState().equals(WorkflowClient.WORKFLOWSTATE_DELETED)) {
-            
-            if (logger.isTraceEnabled()) {
-                logger.trace("The update involved a person authority record. Now checking if updating a collection object is required");
-            }
-
-            if (event.getName().equals(DocumentEventTypes.BEFORE_DOC_UPDATE)) {
-                // Store the previous display name in order to retrieve them in the after the document is created. 
-
-                DocumentModel previousDoc = (DocumentModel) docEventContext.getProperty(CoreEventConstants.PREVIOUS_DOCUMENT_MODEL);
-                // ArrayList previousDisplayName = (ArrayList) previousDoc.getProperty(PERSONS_SCHEMA, DISPLAYNAME_FIELD);
-                String previousDisplayName = (String) previousDoc.getProperty(PERSONS_SCHEMA, DISPLAYNAME_FIELD);
-                
-                docEventContext.setProperty(PREVIOUS_DISPLAYNAME_PROPERTY, previousDisplayName);
-            } else {
-                String previousDisplayName = (String) docEventContext.getProperty(PREVIOUS_DISPLAYNAME_PROPERTY);
-                String newDisplayName = (String) docModel.getProperty(PERSONS_SCHEMA, DISPLAYNAME_FIELD);
-
-                /*
-                 * IMPORTANT: This following bit of code is trying to be smart and is short-circuiting the listener in the event
-                 *          that there is an update to a persons record, but the displayName has not changed. This is to avoid
-                 *          useless updates to collectionobjects records. If a person is used by 30,000 records.
-                 */
-                
-                // if they are equal, we don't need to update the lists
-                if (previousDisplayName.equals(newDisplayName)) {
-                    logger.warn("The version of the Artist Display Name listener currently in use is the optimized version");
-
-                    if (logger.isTraceEnabled()) {
-                        logger.trace("There are no changes to the display field in this record. No updates to any collection object required. " + NO_FURTHER_PROCESSING_MESSAGE);
-                    }
-                    return;
-                }
-              
-                
-                try {
-                    String personCsid = (String) docModel.getName();
-                    // InvocationResults results = updateCollectionObjectsFromPerson(docEventContext).updateNationalitiesFromPerson(personCsid, nationalitiesToUpdate);
-                    InvocationResults results = updateCollectionObjectsFromPerson(docEventContext).UpdateComputedDisplayName(personCsid, newDisplayName);
-                    if (logger.isDebugEnabled()) {
-                    logger.debug("updateParentAccessCode complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
-                    }
-                } catch (Exception e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
-
-        }  else {
-            if (logger.isTraceEnabled()) {
-                logger.trace("No persons record was involved.");
-            }
-            return;
-        }
-    }
 
     protected static boolean documentMatchesType(DocumentModel docModel, String docType) {
         if (docModel == null || Tools.isBlank(docType)) {
@@ -162,5 +91,86 @@ public class UpdateArtistDisplayNameListener implements EventListener {
         
         return updater;
     }
+
+	@Override
+	public boolean shouldHandleEvent(Event event) {
+        EventContext eventContext = event.getContext();
+        if (eventContext == null || !(eventContext instanceof DocumentEventContext)) {
+            return false;
+        }
+
+        DocumentEventContext docEventContext = (DocumentEventContext) eventContext;
+        DocumentModel docModel = docEventContext.getSourceDocument();
+        String docType = docModel.getType();
+
+        // Check if the event involves a person authority
+        if (documentMatchesType(docModel, PERSON_DOCTYPE) &&
+					!documentMatchesType(docModel, "Personauthority") &&
+					!docModel.isVersion() &&
+					!docModel.isProxy() &&
+					!docModel.getCurrentLifeCycleState().equals(WorkflowClient.WORKFLOWSTATE_DELETED)) {
+        	return true;
+        }
+        
+        return false;
+    }
+
+	@Override
+	public void handleCSEvent(Event event) {
+        EventContext eventContext = event.getContext();
+        DocumentEventContext docEventContext = (DocumentEventContext) eventContext;
+        DocumentModel docModel = docEventContext.getSourceDocument();
+        
+        logger.trace("In handleEvent in update nationalities listener.");
+
+        if (logger.isTraceEnabled()) {
+            logger.trace("The update involved a person authority record. Now checking if updating a collection object is required");
+        }
+
+        if (event.getName().equals(DocumentEventTypes.BEFORE_DOC_UPDATE)) {
+            // Store the previous display name in order to retrieve them in the after the document is created. 
+
+            DocumentModel previousDoc = (DocumentModel) docEventContext.getProperty(CoreEventConstants.PREVIOUS_DOCUMENT_MODEL);
+            // ArrayList previousDisplayName = (ArrayList) previousDoc.getProperty(PERSONS_SCHEMA, DISPLAYNAME_FIELD);
+            String previousDisplayName = (String) previousDoc.getProperty(PERSONS_SCHEMA, DISPLAYNAME_FIELD);
+            
+            docEventContext.setProperty(PREVIOUS_DISPLAYNAME_PROPERTY, previousDisplayName);
+        } else {
+            String previousDisplayName = (String) docEventContext.getProperty(PREVIOUS_DISPLAYNAME_PROPERTY);
+            String newDisplayName = (String) docModel.getProperty(PERSONS_SCHEMA, DISPLAYNAME_FIELD);
+
+            /*
+             * IMPORTANT: This following bit of code is trying to be smart and is short-circuiting the listener in the event
+             *          that there is an update to a persons record, but the displayName has not changed. This is to avoid
+             *          useless updates to collectionobjects records. If a person is used by 30,000 records.
+             */
+            
+            // if they are equal, we don't need to update the lists
+            if (previousDisplayName.equals(newDisplayName)) {
+                logger.warn("The version of the Artist Display Name listener currently in use is the optimized version");
+
+                if (logger.isTraceEnabled()) {
+                    logger.trace("There are no changes to the display field in this record. No updates to any collection object required. " + NO_FURTHER_PROCESSING_MESSAGE);
+                }
+                return;
+            }
+            
+            try {
+                String personCsid = (String) docModel.getName();
+                // InvocationResults results = updateCollectionObjectsFromPerson(docEventContext).updateNationalitiesFromPerson(personCsid, nationalitiesToUpdate);
+                InvocationResults results = updateCollectionObjectsFromPerson(docEventContext).UpdateComputedDisplayName(personCsid, newDisplayName);
+                if (logger.isDebugEnabled()) {
+                	logger.debug("updateParentAccessCode complete: numAffected=" + results.getNumAffected() + " userNote=" + results.getUserNote());
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+    }
+
+	@Override
+	protected Log getLogger() {
+		return logger;
+	}
 
 }
