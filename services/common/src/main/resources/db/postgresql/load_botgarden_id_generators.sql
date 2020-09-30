561f208ce92eea32b11c4d11bbb8278db8814fab
Mark Cooper <mark.c.cooper@outlook.com>
2020-09-30 13:56:18 -0700

    DRYD-906: Add Pot Tag Number IdGen

--- services/common/src/main/resources/db/postgresql/load_botgarden_id_generators.sql
+++ services/common/src/main/resources/db/postgresql/load_botgarden_id_generators.sql
@@ -72,8 +72,6 @@ WHERE 'ed3c8578-cf01-4a05-b7fc-8e49b96273c6' NOT IN
       FROM    id_generators
       );
 
-
-
 -- JEPS_ACCESSION_NUMBER
 
 INSERT INTO id_generators
@@ -104,8 +102,6 @@ WHERE 'd9eca382-23b7-47aa-8d9b-d7ff88f49e2b' NOT IN
       SELECT  csid
       FROM    id_generators
       );
-
-
 
 -- VOUCHER_NUMBER
 
@@ -143,7 +139,6 @@ WHERE '940710aa-3df4-4bd4-8d6f-9bf6a92b35cc' NOT IN
       FROM    id_generators
       );
 
-
 -- PROP_NUMBER
 
 INSERT INTO id_generators
@@ -169,3 +164,39 @@ WHERE '81cf5a56-d43d-49e9-ac11-61cf4b0923d4' NOT IN
       SELECT  csid
       FROM    id_generators
       );
+
+-- POTTAG
+
+INSERT INTO id_generators
+    (csid, displayname, description, priority, last_generated_id, id_generator_state)
+  SELECT
+     '3e2e4115-157e-4598-a34b-44224f23c721',
+     'Pot Tag Number',
+     'Reference number for a Pot Tag.',
+     '9',
+     '',
+'<org.collectionspace.services.id.SettableIDGenerator>
+  <parts>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>PT</initialValue>
+      <currentValue>PT</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.YearIDGeneratorPart>
+      <currentValue></currentValue>
+    </org.collectionspace.services.id.YearIDGeneratorPart>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>.</initialValue>
+      <currentValue>.</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.NumericIDGeneratorPart>
+      <maxLength>6</maxLength>
+      <initialValue>1</initialValue>
+      <currentValue>-1</currentValue>
+    </org.collectionspace.services.id.NumericIDGeneratorPart>
+  </parts>
+</org.collectionspace.services.id.SettableIDGenerator>'
+  WHERE '3e2e4115-157e-4598-a34b-44224f23c721' NOT IN
+        (
+        SELECT  csid
+        FROM    id_generators
+        );
