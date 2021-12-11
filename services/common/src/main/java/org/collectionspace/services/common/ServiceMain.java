f9eb56316de6cd276044d8e04c6fe1110e0f3d91
Ray Lee <ray.lee@lyrasis.org>
2021-12-10 19:01:09 -0500

    NOJIRA: Upgrade log4j to 2.15.0.

--- services/common/src/main/java/org/collectionspace/services/common/ServiceMain.java
+++ services/common/src/main/java/org/collectionspace/services/common/ServiceMain.java
@@ -77,7 +77,7 @@ public class ServiceMain {
 	public static final String VER_DISPLAY_NAME = "CollectionSpace Services v7.0";
 	public static final String VER_MAJOR = "7";
 	public static final String VER_MINOR = "0";
-	public static final String VER_PATCH = "0";
+	public static final String VER_PATCH = "1";
 	public static final String VER_BUILD = "1";
 
 	private static final int PRIMARY_REPOSITORY_DOMAIN = 0;
