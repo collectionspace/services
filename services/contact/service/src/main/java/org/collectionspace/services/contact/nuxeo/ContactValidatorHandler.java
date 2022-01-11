c786b7de099dd986844035b6c30bcd8387413ea8
Richard Millet <richard.millet@berkeley.edu>
2022-01-10 16:18:37 -0800

    DRYD-1024: Adding service bindings for hit and audit services.

---
+++ services/contact/service/src/main/java/org/collectionspace/services/contact/nuxeo/ContactValidatorHandler.java
@@ -0,0 +1,62 @@
+package org.collectionspace.services.contact.nuxeo;
+
+import org.collectionspace.services.client.PoxPayloadIn;
+import org.collectionspace.services.client.PoxPayloadOut;
+import org.collectionspace.services.common.document.InvalidDocumentException;
+import org.collectionspace.services.common.document.ValidatorHandlerImpl;
+import org.collectionspace.services.contact.ContactsCommon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ContactValidatorHandler extends ValidatorHandlerImpl<PoxPayloadIn, PoxPayloadOut> {
+
+    final Logger logger = LoggerFactory.getLogger(ContactValidatorHandler.class);
+    //
+    // Error strings
+    //
+    private static final String VALIDATION_ERROR = "The contact record payload was invalid. See log file for more details.";
+
+
+	@Override
+	protected Class<?> getCommonPartClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void handleCreate() throws InvalidDocumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleGet() throws InvalidDocumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleGetAll() throws InvalidDocumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void handleUpdate() throws InvalidDocumentException {
+    	try {
+            ContactsCommon contactsCommon = (ContactsCommon) getCommonPart();
+    	} catch (AssertionError e) {
+    		if (logger.isErrorEnabled() == true) {
+    			logger.error(e.getMessage(), e);
+    		}
+    		throw new InvalidDocumentException(VALIDATION_ERROR, e);
+    	}
+	}
+
+	@Override
+	protected void handleDelete() throws InvalidDocumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
