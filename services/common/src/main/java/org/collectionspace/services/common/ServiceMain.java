782b637c74ab59bfed384c410a6a620d6167f024
Richard Millet <richard.millet@berkeley.edu>
2022-04-01 14:45:51 -0700

    NOJIRA: Config support for customizing which instance of ImageMagick tools get used.  Config support for Audit service.

--- services/common/src/main/java/org/collectionspace/services/common/ServiceMain.java
+++ services/common/src/main/java/org/collectionspace/services/common/ServiceMain.java
@@ -188,7 +188,7 @@ public class ServiceMain {
         return instance;
     }
     
-	static boolean isAuditServiceReady() {
+	static public boolean isAuditServiceReady() {
 		boolean result = true;
 		
 		try {
