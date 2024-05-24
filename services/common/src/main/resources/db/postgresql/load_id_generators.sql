d85000f95d09687364958f3c25607f1c29c0525d
Michael Ritter <emericaskate15@gmail.com>
2024-05-24 15:24:17 -0600

    Summary doc id generator

--- services/common/src/main/resources/db/postgresql/load_id_generators.sql
+++ services/common/src/main/resources/db/postgresql/load_id_generators.sql
@@ -1198,3 +1198,39 @@ INSERT INTO id_generators
         SELECT  csid
         FROM    id_generators
         );
+
+-- SUMMARY_DOCUMENTATION_REFERENCE_NUMBER
+
+INSERT INTO id_generators
+    (csid, displayname, description, priority, last_generated_id, id_generator_state)
+  SELECT
+     '54ab6096-11e0-44a3-9a41-06a87c1c1bdb',
+     'Summary Documentation Reference Number',
+     'Identifies a summary documentation.',
+     '9',
+     '',
+'<org.collectionspace.services.id.SettableIDGenerator>
+  <parts>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>SD</initialValue>
+      <currentValue>SD</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.YearIDGeneratorPart>
+      <currentValue></currentValue>
+    </org.collectionspace.services.id.YearIDGeneratorPart>
+    <org.collectionspace.services.id.StringIDGeneratorPart>
+      <initialValue>.</initialValue>
+      <currentValue>.</currentValue>
+    </org.collectionspace.services.id.StringIDGeneratorPart>
+    <org.collectionspace.services.id.NumericIDGeneratorPart>
+      <maxLength>6</maxLength>
+      <initialValue>1</initialValue>
+      <currentValue>-1</currentValue>
+    </org.collectionspace.services.id.NumericIDGeneratorPart>
+  </parts>
+</org.collectionspace.services.id.SettableIDGenerator>'
+  WHERE '54ab6096-11e0-44a3-9a41-06a87c1c1bdb' NOT IN
+        (
+        SELECT  csid
+        FROM    id_generators
+        );
