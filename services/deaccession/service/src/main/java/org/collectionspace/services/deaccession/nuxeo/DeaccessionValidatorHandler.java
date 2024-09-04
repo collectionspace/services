cc933a925c46afb15a1e9600b897614f8fdd1e62
Michael Ritter <mike.ritter@lyrasis.org>
2024-09-04 16:34:45 -0600

    Implement validator handler

--- services/deaccession/service/src/main/java/org/collectionspace/services/deaccession/nuxeo/DeaccessionValidatorHandler.java
+++ services/deaccession/service/src/main/java/org/collectionspace/services/deaccession/nuxeo/DeaccessionValidatorHandler.java
@@ -18,36 +18,47 @@ import org.collectionspace.services.client.PoxPayloadIn;
 import org.collectionspace.services.client.PoxPayloadOut;
 import org.collectionspace.services.common.document.InvalidDocumentException;
 import org.collectionspace.services.common.document.ValidatorHandlerImpl;
+import org.collectionspace.services.deaccession.DeaccessionsCommon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeaccessionValidatorHandler extends ValidatorHandlerImpl<PoxPayloadIn, PoxPayloadOut> {
 
+    private final Logger logger = LoggerFactory.getLogger(DeaccessionValidatorHandler.class);
+
+    private static final String COMMON_PART_MISSING = "Validation exception: deaccessions_common part is missing";
+    private static final String DEACCESSION_NUMBER_MISSING =
+        "Validation exception: The field \"deaccessionNumber\" cannot be empty or missing";
+
     @Override
     protected Class<?> getCommonPartClass() {
-        return null;
+        return DeaccessionsCommon.class;
     }
 
     @Override
     protected void handleCreate() throws InvalidDocumentException {
+        final DeaccessionsCommon common = (DeaccessionsCommon) getCommonPart();
+        if (common == null) {
+            logger.error(COMMON_PART_MISSING);
+            throw new InvalidDocumentException(COMMON_PART_MISSING);
+        }
 
+        final String deaccessionNumber = common.getDeaccessionNumber();
+        if (deaccessionNumber == null || deaccessionNumber.isEmpty()) {
+            logger.error(DEACCESSION_NUMBER_MISSING);
+            throw new InvalidDocumentException(DEACCESSION_NUMBER_MISSING);
+        }
     }
 
     @Override
-    protected void handleGet() throws InvalidDocumentException {
-
-    }
+    protected void handleGet() {}
 
     @Override
-    protected void handleGetAll() throws InvalidDocumentException {
-
-    }
+    protected void handleGetAll() {}
 
     @Override
-    protected void handleUpdate() throws InvalidDocumentException {
-
-    }
+    protected void handleUpdate() {}
 
     @Override
-    protected void handleDelete() throws InvalidDocumentException {
-
-    }
+    protected void handleDelete() {}
 }
