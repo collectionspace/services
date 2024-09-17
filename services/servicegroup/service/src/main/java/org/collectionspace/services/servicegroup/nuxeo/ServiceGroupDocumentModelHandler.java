5afacd2dfe54fcd81d3e33398ce4f3594ceab624
Michael Ritter <mike.ritter@lyrasis.org>
2024-09-17 17:21:54 -0600

    Allow multiple tags and negation tags as query parameters

--- services/servicegroup/service/src/main/java/org/collectionspace/services/servicegroup/nuxeo/ServiceGroupDocumentModelHandler.java
+++ services/servicegroup/service/src/main/java/org/collectionspace/services/servicegroup/nuxeo/ServiceGroupDocumentModelHandler.java
@@ -78,32 +78,32 @@ import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ServiceGroupDocumentModelHandler 
+public class ServiceGroupDocumentModelHandler
 	extends NuxeoDocumentModelHandler<ServicegroupsCommon> {
-	
+
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
     protected static final int NUM_META_FIELDS = 4; // the number of meta fields -i.e., DOC_TYPE_FIELD, DOC_NUMBER_FIELD, DOC_NAME_FIELD, STANDARD_LIST_MARK_RT_FIELD
     protected static final String DOC_TYPE_FIELD = "docType";
     protected static final String DOC_NUMBER_FIELD = "docNumber";
     protected static final String DOC_NAME_FIELD = "docName";
 	protected static final String STANDARD_LIST_MARK_RT_FIELD = "related";
-    
+
     //
     // Returns a service payload for an authority item
     //
     private PoxPayloadOut getAuthorityItem(ServiceContext ctx, String termRefName) throws Exception {
     	PoxPayloadOut result = null;
-    	
+
     	RefName.AuthorityItem item = RefName.AuthorityItem.parse(termRefName, true);
     	AuthorityResource authorityResource = (AuthorityResource) ctx.getResourceMap().get(item.inAuthority.resource);
-    	
+
     	AuthorityTermInfo authorityTermInfo = RefNameUtils.parseAuthorityTermInfo(termRefName);
     	String parentIdentifier = Specifier.createShortIdURNValue(authorityTermInfo.inAuthority.name);
     	String itemIdentifier = Specifier.createShortIdURNValue(authorityTermInfo.name);
-    	
+
     	result = authorityResource.getAuthorityItemWithExistingContext(ctx, parentIdentifier, itemIdentifier);
-    	
+
     	return result;
     }
 
@@ -113,8 +113,8 @@ public class ServiceGroupDocumentModelHandler
     	PoxPayloadOut result = null;
         CoreSessionInterface repoSession = null;
     	boolean releaseRepoSession = false;
-        
-    	try { 
+
+    	try {
     		NuxeoRepositoryClientImpl repoClient = (NuxeoRepositoryClientImpl)this.getRepositoryClient(ctx);
     		repoSession = this.getRepositorySession();
     		if (repoSession == null) {
@@ -135,7 +135,7 @@ public class ServiceGroupDocumentModelHandler
     	    	String termRefName = (String) NuxeoUtils.getProperyValue(docModel, CollectionSpaceClient.COLLECTIONSPACE_CORE_REFNAME);
     	        if (isAuthorityTermDocument(termRefName) == true) {
                 	result = getAuthorityItem(ctx, termRefName);
-    	        } else {    	        
+    	        } else {
 	                TenantBindingConfigReaderImpl bindingReader = ServiceMain.getInstance().getTenantBindingConfigReader();
 	                String serviceName = ServiceBindingUtils.getServiceNameFromObjectName(bindingReader, ctx.getTenantId(),
 	                		docModel.getDocumentType().getName());
@@ -160,13 +160,13 @@ public class ServiceGroupDocumentModelHandler
     		}
     		throw new DocumentException(e);
     	}
-    	
+
         return result;
     }
-    
+
     private boolean isAuthorityTermDocument(String termRefName) {
 		boolean result = true;
-		
+
 		try {
 	    	//String inAuthorityCsid = (String) NuxeoUtils.getProperyValue(docModel, "inAuthority"); //docModel.getPropertyValue("inAuthority"); // AuthorityItemJAXBSchema.IN_AUTHORITY
 	    	//String refName = (String) NuxeoUtils.getProperyValue(docModel, CollectionSpaceClient.COLLECTIONSPACE_CORE_REFNAME);
@@ -174,7 +174,7 @@ public class ServiceGroupDocumentModelHandler
 		} catch (IllegalArgumentException e) {
 			result = false;
 		}
-		
+
     	return result;
 	}
 
@@ -215,17 +215,14 @@ public class ServiceGroupDocumentModelHandler
 			throw new CSWebApplicationException(response);
 		}
 
-		String tag = ctx.getQueryParams().getFirst(IQueryManager.TAG_QUERY_PARAM);
+		List<String> queryTags = ctx.getQueryParams().get(IQueryManager.TAG_QUERY_PARAM);
 		servicebindings = SecurityUtils.getReadableServiceBindingsForCurrentUser(servicebindings);
 		// Build the list of docTypes for allowed serviceBindings filtered optionally on tags
 		ArrayList<String> docTypes = new ArrayList<String>();
 		for (ServiceBindingType binding : servicebindings) {
-			boolean acceptDocType = false;
-			Tags tags = binding.getTags();
-			if (tag == null) {
-				acceptDocType = true;
-			} else if (tags != null && !tags.getTag().isEmpty()) {
-				acceptDocType = tags.getTag().contains(tag);
+			boolean acceptDocType = true;
+			if (queryTags != null && !queryTags.isEmpty()) {
+				acceptDocType = acceptServiceBinding(binding, queryTags);
 			}
 
 			ServiceObjectType serviceObj = binding.getObject();
@@ -239,7 +236,7 @@ public class ServiceGroupDocumentModelHandler
 		// we can't query if no doc types match, likely from a bad tag
 		if (docTypes.isEmpty()) {
 			Response response = Response.status(Response.Status.BAD_REQUEST)
-				.entity("Cannot query against no document types. Check that tag " + tag + " is correct.")
+				.entity("Cannot query against no document types. Check that tag query parameters are correct.")
 				.type(MediaType.TEXT_PLAIN_TYPE)
 				.build();
 			throw new CSWebApplicationException(response);
@@ -259,6 +256,37 @@ public class ServiceGroupDocumentModelHandler
 		return docList;
 	}
 
+	/**
+	 * Test if a service binding should be used in a query based on the tag query parameters passed in.
+	 * <p>
+	 * If a tag starts with a "-", it is assumed this is intended to be a negation, and we check if the service binding
+	 * does not contain said tag.
+	 *
+	 * @param binding the ServiceBinding to check
+	 * @param queryTags the list of tag query parameters
+	 * @return true if the ServiceBinding contains all query parameters, false otherwise
+	 */
+	public boolean acceptServiceBinding(ServiceBindingType binding, List<String> queryTags) {
+		final Tags tags = binding.getTags();
+		if (tags == null || tags.getTag().isEmpty()) {
+			return false;
+		}
+
+		boolean accept = true;
+		// todo: what about empty strings? can they come through?
+		final List<String> tagList = tags.getTag();
+		for (String query : queryTags) {
+			if (query.startsWith("-")) {
+				query = query.substring(1);
+				accept &= !tagList.contains(query);
+			} else {
+				accept &= tagList.contains(query);
+			}
+		}
+
+		return accept;
+	}
+
     public AbstractCommonList getItemListForGroup(
     		ServiceContext<PoxPayloadIn, PoxPayloadOut> ctx,
     		List<String> serviceGroupNames) throws Exception {
@@ -266,8 +294,8 @@ public class ServiceGroupDocumentModelHandler
         AbstractCommonList list = (AbstractCommonList)commonList;
         CoreSessionInterface repoSession = null;
     	boolean releaseRepoSession = false;
-        
-    	try { 
+
+    	try {
             DocumentFilter myFilter = getDocumentFilter();
 	        int pageSize = myFilter.getPageSize();
 	        int pageNum = myFilter.getStartPage();
@@ -308,14 +336,14 @@ public class ServiceGroupDocumentModelHandler
     		}
     		throw new DocumentException(e);
     	}
-    	
+
         return list;
     }
-    
+
     // Move this to a Utils class!
     public static String implode(List<String> stringList, String sep) {
     	StringBuilder sb = new StringBuilder();
-    	
+
     	boolean fFirst = false;
     	for (String name:stringList) {
     		if(fFirst) {
@@ -325,30 +353,30 @@ public class ServiceGroupDocumentModelHandler
     		}
     		sb.append(name);
     	}
-    	
+
     	return sb.toString();
     }
-    
+
     private String getUriFromServiceBinding(ServiceBindingType sb, String csid) {
         return "/" + sb.getName().toLowerCase() + "/" + csid;
     }
-    
+
     private void processDocList(ServiceContext<PoxPayloadIn, PoxPayloadOut> ctx,
 			DocumentModelList docList,
 			Map<String, ServiceBindingType> queriedServiceBindings,
 			CommonList list) throws DocumentException {
-    	
+
 		String tenantId = ctx.getTenantId();
 		CoreSessionInterface repoSession = null;
 		NuxeoRepositoryClientImpl repoClient = null;
 		boolean releaseRepoSession = false;
-		
+
 		MultivaluedMap<String, String> queryParams = getServiceContext().getQueryParams();
 		String markRtSbj = queryParams.getFirst(IQueryManager.MARK_RELATED_TO_CSID_AS_SUBJECT);
 		if (Tools.isBlank(markRtSbj)) {
 			markRtSbj = null;
 		}
-		
+
 		String markRtSbjOrObj = queryParams.getFirst(IQueryManager.MARK_RELATED_TO_CSID_AS_EITHER);
 		if (Tools.isBlank(markRtSbjOrObj)) {
 			markRtSbjOrObj = null;
@@ -359,7 +387,7 @@ public class ServiceGroupDocumentModelHandler
 			}
 			markRtSbj = markRtSbjOrObj; // Mark the record as related independent of whether it is the subject or object of a relationship
 		}
-		
+
 		try {
 			if (markRtSbj != null) {
 				repoClient = (NuxeoRepositoryClientImpl) this.getRepositoryClient(ctx);
@@ -370,7 +398,7 @@ public class ServiceGroupDocumentModelHandler
 					releaseRepoSession = true;
 				}
 			}
-	    				
+
 	        String fields[] = new String[NUM_META_FIELDS + NUM_STANDARD_LIST_RESULT_FIELDS];
 	        fields[0] = STANDARD_LIST_CSID_FIELD;
 	        fields[1] = STANDARD_LIST_URI_FIELD;
@@ -379,13 +407,13 @@ public class ServiceGroupDocumentModelHandler
 	        fields[4] = STANDARD_LIST_REFNAME_FIELD;
 	        fields[5] = DOC_NAME_FIELD;
 	        fields[6] = DOC_NUMBER_FIELD;
-	        fields[7] = DOC_TYPE_FIELD;	        
+	        fields[7] = DOC_TYPE_FIELD;
 			if (markRtSbj != null) {
 		        fields[8] = STANDARD_LIST_MARK_RT_FIELD;
 			}
 
 	        list.setFieldsReturned(fields);
-	        
+
 	        Iterator<DocumentModel> iter = docList.iterator();
 			HashMap<String, Object> item = new HashMap<String, Object>();
 	        while (iter.hasNext()) {
@@ -396,7 +424,7 @@ public class ServiceGroupDocumentModelHandler
 	            if (sb == null) {
 	                throw new RuntimeException("processDocList: No Service Binding for docType: " + docType);
 	            }
-	            
+
 	            String csid = NuxeoUtils.getCsid(docModel);
 	            item.put(STANDARD_LIST_CSID_FIELD, csid);
 
@@ -417,7 +445,7 @@ public class ServiceGroupDocumentModelHandler
 					item.put(STANDARD_LIST_MARK_RT_FIELD, queryDocList.isEmpty() ? "false" : "true");
 				}
 
-	            UriTemplateRegistry uriTemplateRegistry = ServiceMain.getInstance().getUriTemplateRegistry();            
+	            UriTemplateRegistry uriTemplateRegistry = ServiceMain.getInstance().getUriTemplateRegistry();
 	            StoredValuesUriTemplate storedValuesResourceTemplate = uriTemplateRegistry.get(new UriTemplateRegistryKey(tenantId, docType));
 		 	    Map<String, String> additionalValues = new HashMap<String, String>();
 		 	    if (storedValuesResourceTemplate.getUriTemplateType() == UriTemplateFactory.ITEM) {
@@ -432,7 +460,7 @@ public class ServiceGroupDocumentModelHandler
 		 	    } else {
 	                additionalValues.put(UriTemplateFactory.IDENTIFIER_VAR, csid);
 	            }
-		 	    
+
 	            String uriStr = storedValuesResourceTemplate.buildUri(additionalValues);
 	            item.put(STANDARD_LIST_URI_FIELD, uriStr);
 	            try {
@@ -442,17 +470,17 @@ public class ServiceGroupDocumentModelHandler
 	            } catch(Exception e) {
 	            	logger.error("Error getting core values for doc ["+csid+"]: "+e.getLocalizedMessage());
 	            }
-	
+
 	            String value = ServiceBindingUtils.getMappedFieldInDoc(sb, ServiceBindingUtils.OBJ_NUMBER_PROP, docModel);
 	            if (value != null) {
 	            	item.put(DOC_NUMBER_FIELD, value);
 	            }
-	            
+
 	            value = ServiceBindingUtils.getMappedFieldInDoc(sb, ServiceBindingUtils.OBJ_NAME_PROP, docModel);
 	            if (value != null) {
 	            	item.put(DOC_NAME_FIELD, value);
 	            }
-	            
+
 	            item.put(DOC_TYPE_FIELD, docType);
 	            // add the item to the list
 	            list.addItem(item);
