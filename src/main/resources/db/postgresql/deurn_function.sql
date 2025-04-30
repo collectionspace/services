b32ee6852eba013c35900bdd27f95c9e55c418c9
Anthony Bucci <abucci@bucci.onl>
2025-04-30 14:18:02 -0400

    DRYD-1748: bugfixes to syntax of deurn_function.sql

--- src/main/resources/db/postgresql/deurn_function.sql
+++ src/main/resources/db/postgresql/deurn_function.sql
@@ -1,7 +1,3 @@
 -- Convert a URN (often refname) (DRYD-1748: Add deurn function to database)
-CREATE OR REPLACE FUNCTION deurn(urnstr VARCHAR)
-RETURNS VARCHAR AS $$
-BEGIN
-    RETURN (regexp_match(urnstr, '''([^''\\]*(\\.[^''\\]*)*)'''))[1];
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION deurn(urnstr VARCHAR) RETURNS VARCHAR
+AS $BODY$ BEGIN RETURN ((regexp_match(urnstr, '''([^''\\]*(\\.[^''\\]*)*)'''))[1]); END $BODY$ LANGUAGE plpgsql IMMUTABLE LEAKPROOF;
