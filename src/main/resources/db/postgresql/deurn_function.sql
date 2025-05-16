3c7a411fa4f30bd3775cef29b8f3e717217cb0d6
Anthony Bucci <abucci@bucci.onl>
2025-05-16 10:17:28 -0400

    DRYD-1748: removed LEAKPROOF from deurn_function.sql

--- src/main/resources/db/postgresql/deurn_function.sql
+++ src/main/resources/db/postgresql/deurn_function.sql
@@ -1,3 +1,3 @@
 -- Convert a URN (often refname) (DRYD-1748: Add deurn function to database)
 CREATE OR REPLACE FUNCTION deurn(urnstr VARCHAR) RETURNS VARCHAR
-AS $BODY$ BEGIN RETURN ((regexp_match(urnstr, '''([^''\\]*(\\.[^''\\]*)*)'''))[1]); END $BODY$ LANGUAGE plpgsql IMMUTABLE LEAKPROOF;
+AS $BODY$ BEGIN RETURN ((regexp_match(urnstr, '''([^''\\]*(\\.[^''\\]*)*)'''))[1]); END $BODY$ LANGUAGE plpgsql IMMUTABLE;
