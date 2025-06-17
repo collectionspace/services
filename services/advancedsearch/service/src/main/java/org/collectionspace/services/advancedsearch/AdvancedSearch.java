01c324f319c4dc3776a8839acf5820ec9c9db484
Anthony Bucci <abucci@bucci.onl>
2025-06-17 15:02:55 -0400

    fixing compilation error; sketch of calling another service

--- services/advancedsearch/service/src/main/java/org/collectionspace/services/advancedsearch/AdvancedSearch.java
+++ services/advancedsearch/service/src/main/java/org/collectionspace/services/advancedsearch/AdvancedSearch.java
@@ -8,16 +8,22 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 
 import org.collectionspace.services.client.AdvancedSearchClient;
-import org.collectionspace.services.client.IClientQueryParams;
+//import org.collectionspace.services.client.IClientQueryParams;
+import org.collectionspace.services.collectionobject.CollectionObjectResource;
 import org.collectionspace.services.jaxb.AbstractCommonList;
+
 
 @Path(AdvancedSearchClient.SERVICE_PATH)
 @Consumes("application/xml")
 @Produces("application/xml")
 public class AdvancedSearch {
+	private CollectionObjectResource cor = new CollectionObjectResource();
+	
 	@GET
 	public AbstractCommonList getList(@Context UriInfo uriInfo) {
 		AbstractCommonList results = null;
+		
+		AbstractCommonList foo = cor.getList(uriInfo);
 		
 		return results;
 	}
