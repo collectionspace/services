c50d485c2990cb04cc4b9d3e41e58341a9c4ea90
Anthony Bucci <abucci@bucci.onl>
2025-06-17 12:52:31 -0400

    tinkering with AdvancedSearch

--- services/advancedsearch/service/src/main/java/org/collectionspace/services/advancedsearch/AdvancedSearch.java
+++ services/advancedsearch/service/src/main/java/org/collectionspace/services/advancedsearch/AdvancedSearch.java
@@ -1,3 +1,25 @@
 package org.collectionspace.services.advancedsearch; 
 
-public class AdvancedSearch {}
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+import org.collectionspace.services.client.AdvancedSearchClient;
+import org.collectionspace.services.client.IClientQueryParams;
+import org.collectionspace.services.jaxb.AbstractCommonList;
+
+@Path(AdvancedSearchClient.SERVICE_PATH)
+@Consumes("application/xml")
+@Produces("application/xml")
+public class AdvancedSearch {
+	@GET
+	public AbstractCommonList getList(@Context UriInfo uriInfo) {
+		AbstractCommonList results = null;
+		
+		return results;
+	}
+	
+}
