e4c2345ff877db7b3f33d2aec706d29e2e22e688
Michael Ritter <mike.ritter@lyrasis.org>
2026-07-14 13:17:56 -0600

    Remove unused imports

--- services/account/service/src/main/java/org/collectionspace/services/account/AccountResource.java
+++ services/account/service/src/main/java/org/collectionspace/services/account/AccountResource.java
@@ -35,8 +35,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
