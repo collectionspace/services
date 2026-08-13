4dad4ebd748cad0d8f49dac3aa2bbbddfb8122fb
Michael Ritter <mike.ritter@lyrasis.org>
2026-08-13 11:03:19 -0600

    Rename + add comments

--- services/common/src/main/resources/db/postgresql/fulltext_triggers.sql
+++ services/common/src/main/resources/db/postgresql/fulltext_triggers.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE FUNCTION fulltext_rm_punctuation() RETURNS TRIGGER AS $$
+-- Remove punctuation from fulltext columns
+-- Note: There is a nuxeo trigger which sets the fulltext.fulltext column by coalescing the simpletext and binarytext.
+CREATE OR REPLACE FUNCTION fulltext_normalize() RETURNS TRIGGER AS $$
 BEGIN
   NEW.simpletext = regexp_replace(NEW.simpletext, '[^\w]+', ' ', 'gi');
   NEW.binarytext = regexp_replace(NEW.binarytext, '[^\w]+', ' ', 'gi');
@@ -6,8 +8,8 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS fulltext_rm_punct_trigger ON fulltext;
-CREATE TRIGGER fulltext_rm_punct_trigger
+DROP TRIGGER IF EXISTS fulltext_normalize_trigger ON fulltext;
+CREATE TRIGGER fulltext_normalize_trigger
 BEFORE INSERT OR UPDATE ON fulltext
 FOR EACH ROW
-EXECUTE FUNCTION fulltext_rm_punctuation();
+EXECUTE FUNCTION fulltext_normalize();
